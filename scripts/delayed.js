239ff9ffce86e7dcb272cc2fab5872f6dd0e232e
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-02-27 00:05:17 +0100

    5 - Product Carousel

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,15 +1,7 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from "./lib-franklin.js";
+import { sampleRUM } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
-sampleRUM("cwv");
+sampleRUM('cwv');
 
 // add more delayed functionality here
-
-/* TODO - Move this to a better place */
-document.querySelectorAll(".carousel-item.selected").forEach((item) => {
-  item.parentNode.scrollTo({
-    top: 0,
-    left: item.offsetLeft - item.parentNode.offsetLeft - 200,
-  });
-});
