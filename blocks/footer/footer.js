62f5e9b3d9381b1b9efaf944ea8b546b114ab5b6
Paolo <paolo@pc-52.home>
2023-02-27 17:21:22 +0100

    footer news events switch

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,29 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
+function switchGroup(selectionTitle, position) {
+    var i;
+    var div;
+    
+    // Set active and inactive selection title
+    for (i = 0; i < selectionTitle.parentElement.children.length; i++) {
+        div = selectionTitle.parentElement.children[i];
+        div.classList.remove('on');
+        div.classList.add('off');
+    }    
+    selectionTitle.classList.add('on');
+    selectionTitle.classList.remove('off');
+    
+    // Set active and inactive content div
+    var blockDiv = selectionTitle.parentElement.parentElement.parentElement;
+    for (i = 1; i < blockDiv.children.length; i++) {
+        div = blockDiv.children[i];
+        div.classList.add('off');
+        div.classList.remove('on');
+    }
+    blockDiv.children[position].classList.add('on');
+    blockDiv.children[position].classList.remove('off');
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The header block element
@@ -14,6 +38,32 @@ export default async function decorate(block) {
   const html = await resp.text();
   const footer = document.createElement('div');
   footer.innerHTML = html;
+    
+  let selectionDiv = footer.querySelector('div:nth-of-type(2)');
+    
+
+        
+  let newsTitle = selectionDiv.querySelector('h3:nth-of-type(1)');
+  newsTitle.innerHTML = "<a>" + newsTitle.textContent + "</a>";
+  newsTitle.querySelector('a').addEventListener('click', function() {
+      switchGroup(newsTitle, 1);
+  }, false);
+  
+  let eventsTitle = selectionDiv.querySelector('h3:nth-of-type(2)');
+  eventsTitle.innerHTML = "<a>" + eventsTitle.textContent + "</a>";
+  eventsTitle.querySelector('a').addEventListener('click', function() {
+      switchGroup(eventsTitle, 2);
+  }, false);
+    
+   var blockDiv = newsTitle.parentElement.parentElement.parentElement;
+    for (var i = 1; i < blockDiv.children.length; i++) {
+        var div = blockDiv.children[i];
+        var pars = div.querySelectorAll("p");
+        pars.item(pars.length - 1).innerHTML +=
+            "<span class='icon-icon_link'></span>";
+    }
+    
   await decorateIcons(footer);
   block.append(footer);
 }
+
