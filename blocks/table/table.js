f15c58f01a9092e11f4cef089201c5d45ab6fe06
puric <puric@adobe.com>
2023-03-03 16:39:04 +0100

    add more alignment styles and allow simple merging

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,21 +1,45 @@
-function buildCell(rowIndex, dataAlign) {
+function buildCell(rowIndex, colspan, dataAlign, dataVAlign) {
     const cell = rowIndex ? document.createElement('td') : document.createElement('th');
-    if (!rowIndex) cell.setAttribute('scope', 'col');
-    if (dataAlign) cell.className = dataAlign;
+    if (colspan > 0) cell.setAttribute('colspan', colspan);
+    if (dataAlign) cell.classList.add('text-align-' + dataAlign);
+    if (dataVAlign) cell.classList.add('vertical-align-' + dataVAlign);
     return cell;
   }
-  
+
+function getNumberOfColumns(block) {
+  // get max children length for colspan
+  let maxCols = 1;
+  [...block.children].forEach((child, i) => {
+    if (child.children.length > 1) maxCols = child.children.length
+  });
+  return maxCols;
+}
+
+function calculateColspan(colIndex, cols, maxCols) {
+  // calculate how many columns have to be merged
+  let colspan = 0
+  if ((cols != maxCols) && (cols == colIndex + 1)) {
+    colspan = maxCols - colIndex;
+  };
+  return colspan;
+}
+
   export default async function decorate(block) {
     const table = document.createElement('table');
     const thead = document.createElement('thead');
     const tbody = document.createElement('tbody');
     table.append(thead, tbody);
+
+    let cols = getNumberOfColumns(block);
     [...block.children].forEach((child, i) => {
       const row = document.createElement('tr');
       if (i) tbody.append(row);
       else thead.append(row);
-      [...child.children].forEach((col) => {
-        const cell = buildCell(i, col.getAttribute('data-align'));
+      [...child.children].forEach((col, j) => {
+        let colspan = calculateColspan(j, child.children.length, cols);
+        let dataAlign = col.getAttribute('data-align');
+        let dataVAlign = col.getAttribute('data-valign');
+        const cell = buildCell(i, colspan, dataAlign, dataVAlign);
         cell.innerHTML += col.innerHTML;
         row.append(cell);
       });
