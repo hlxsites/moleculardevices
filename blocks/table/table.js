d106e825fe020fec9f64d58bc2c8116d4cc8ae11
Lydia Puric <lydia.puric@gmail.com>
2023-03-03 20:30:57 +0100

    Fix lint issues

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,10 +1,10 @@
 function buildCell(rowIndex, colspan, dataAlign, dataVAlign) {
-    const cell = rowIndex ? document.createElement('td') : document.createElement('th');
-    if (colspan > 0) cell.setAttribute('colspan', colspan);
-    if (dataAlign) cell.classList.add('text-align-' + dataAlign);
-    if (dataVAlign) cell.classList.add('vertical-align-' + dataVAlign);
-    return cell;
-  }
+  const cell = rowIndex ? document.createElement('td') : document.createElement('th');
+  if (colspan > 0) cell.setAttribute('colspan', colspan);
+  if (dataAlign) cell.classList.add('text-align-' + dataAlign);
+  if (dataVAlign) cell.classList.add('vertical-align-' + dataVAlign);
+  return cell;
+}
 
 function getNumberOfColumns(block) {
   // get max children length for colspan
@@ -24,29 +24,29 @@ function calculateColspan(colIndex, cols, maxCols) {
   return colspan;
 }
 
-  export default async function decorate(block) {
-    const table = document.createElement('table');
-    const thead = document.createElement('thead');
-    const tbody = document.createElement('tbody');
-    table.append(thead, tbody);
+export default async function decorate(block) {
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+  table.append(thead, tbody);
 
-    let cols = getNumberOfColumns(block);
-    [...block.children].forEach((child, i) => {
-      const row = document.createElement('tr');
-      if (i) {
-        tbody.append(row);
-      } else {
-        thead.append(row);
-      }
-      [...child.children].forEach((col, j) => {
-        let colspan = calculateColspan(j, child.children.length, cols);
-        let dataAlign = col.getAttribute('data-align');
-        let dataVAlign = col.getAttribute('data-valign');
-        const cell = buildCell(i, colspan, dataAlign, dataVAlign);
-        cell.innerHTML += col.innerHTML;
-        row.append(cell);
-      });
+  let cols = getNumberOfColumns(block);
+  [...block.children].forEach((child, i) => {
+    const row = document.createElement('tr');
+    if (i) {
+       tbody.append(row);
+    } else {
+      thead.append(row);
+    }
+    [...child.children].forEach((col, j) => {
+      let colspan = calculateColspan(j, child.children.length, cols);
+      let dataAlign = col.getAttribute('data-align');
+      let dataVAlign = col.getAttribute('data-valign');
+      const cell = buildCell(i, colspan, dataAlign, dataVAlign);
+      cell.innerHTML += col.innerHTML;
+      row.append(cell);
     });
-    block.innerHTML = '';
-    block.append(table);
-  }
+  });
+  block.innerHTML = '';
+  block.append(table);
+}
