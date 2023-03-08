387d5e62b01947a629f106677adfca7a316acb44
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-03-08 11:57:06 +0100

    4 - Hero Video

--- test/hero-video/hero-video.test.js
+++ test/hero-video/hero-video.test.js
@@ -1,28 +1,28 @@
+/* eslint-disable no-unused-expressions */
+/* global describe it */
+
 import { readFile } from '@web/test-runner-commands';
-import { expect } from "@esm-bundle/chai";
+import { expect } from '@esm-bundle/chai';
 
-const sleep = async (time = 1000) =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, time);
-  });
+const sleep = async (time = 1000) => new Promise((resolve) => {
+  setTimeout(() => {
+    resolve(true);
+  }, time);
+});
 
-describe("Hero Video Block", () => {
+describe('Hero Video Block', () => {
   // eslint-disable-next-line no-undef
   before(async () => {
-    const { decorateBlock, loadBlock } = await import(
-      "../../../scripts/lib-franklin.js"
-    );
-    document.body.innerHTML = await readFile({ path: "./block.html" });
-    const tableBlock = document.querySelector("div.hero-video-wrapper .hero-video");
+    const { decorateBlock, loadBlock } = await import('../../../scripts/lib-franklin.js');
+    document.body.innerHTML = await readFile({ path: './block.html' });
+    const tableBlock = document.querySelector('div.hero-video-wrapper .hero-video');
     await decorateBlock(tableBlock);
     await loadBlock(tableBlock);
     await sleep();
   });
 
-  it("Block structure is correct", async () => {
-    const block = document.querySelector("div.hero-video-wrapper .hero-video");
+  it('Block structure is correct', async () => {
+    const block = document.querySelector('div.hero-video-wrapper .hero-video');
     const videos = block.querySelectorAll('video');
     expect(videos.length).to.equal(2);
     expect(block.querySelector('.hero-video-banner'), 'Video Banner container is missing').to.exist;
@@ -32,7 +32,7 @@ describe("Hero Video Block", () => {
 
     const teaserVideoContainer = block.querySelector('.teaser-video-container');
     // the video should be before the image
-    expect(teaserVideoContainer.children[0].tagName).to.equal('VIDEO'); 
+    expect(teaserVideoContainer.children[0].tagName).to.equal('VIDEO');
     expect(teaserVideoContainer.children[1].tagName).to.equal('PICTURE');
 
     expect(block.querySelector('.overlay button'), 'Overlay Button Missing').to.exist;
