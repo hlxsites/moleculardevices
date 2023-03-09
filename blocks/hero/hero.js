a57a3ff33714d2e32fa3a408a9a368676e5c75c3
Paolo Mottadelli <paolom@adobe.com>
2023-03-09 19:00:50 +0100

    hero mobile and 2 columns

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,6 +1,15 @@
 export default function decorate(block) {
+
+  const container = document.createElement('div');
+  container.classList.add('container');
+
+  [...block.children].forEach((div, index) => {
+    container.appendChild(div);
+  });
+
+  block.appendChild(container);
+
   const picture = block.querySelector('picture');
-  if (picture) {
-    block.prepend(picture.parentElement);
-  }
+  block.prepend(picture.parentElement);
+
 }
