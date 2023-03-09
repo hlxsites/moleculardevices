c82c6ee8e5b37eb2fbbdfd0bc067653d7ea8ffa4
Paolo Mottadelli <paolom@adobe.com>
2023-03-09 16:31:14 +0100

    fix eslint errors

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,6 +1,6 @@
 export default function decorate(block) {
   const picture = block.querySelector('picture');
   if (picture) {
-    block.prepend(picture.parentElement); 
+    block.prepend(picture.parentElement);
   }
 }
