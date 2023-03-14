88d3b2382e4bb05e8ac079546c43a53eeae73822
kapildhiman <kapil.php@gmail.com>
2023-03-14 18:09:59 +0530

    added lazy lading for iframes

--- blocks/get-in-touch/get-in-touch.js
+++ blocks/get-in-touch/get-in-touch.js
@@ -1,12 +1,14 @@
-const hubspotUrl = document.querySelector('.get-in-touch-wrapper > div > div > div:first-of-type a');
+const hubspotUrl = document.querySelector('.get-in-touch-wrapper > div > div > div:first-of-type [href*="https://info.moleculardevices.com"]');
 const hubspotIframeWrapper = document.createElement('div');
 const hubspotIframe = document.createElement('iframe');
 hubspotIframeWrapper.className = 'hubspot-iframe-wrapper';
 hubspotIframe.src = hubspotUrl.href;
+hubspotIframe.setAttribute('loading', 'lazy');
 hubspotIframeWrapper.appendChild(hubspotIframe);
 hubspotUrl.parentNode.replaceChild(hubspotIframeWrapper, hubspotUrl);
 
-const mapUrl = document.querySelector('.get-in-touch-wrapper > div > div > div:last-of-type a:nth-child(1)');
+const mapUrl = document.querySelector('.get-in-touch-wrapper > div > div > div:last-of-type [href*="https://maps.google.com"]');
 const mapIframe = document.createElement('iframe');
 mapIframe.src = mapUrl.href;
+mapIframe.setAttribute('loading', 'lazy');
 mapUrl.parentNode.replaceChild(mapIframe, mapUrl);
