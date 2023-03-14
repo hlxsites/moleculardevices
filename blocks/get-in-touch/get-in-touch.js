4c2911f8423ae84c7a1509bf4dc535a17f3cd3d9
kapildhiman <kapil.php@gmail.com>
2023-03-14 17:40:28 +0530

    test wihtout iframe

--- blocks/get-in-touch/get-in-touch.js
+++ blocks/get-in-touch/get-in-touch.js
@@ -1,12 +1,12 @@
 const hubspotUrl = document.querySelector('.get-in-touch-wrapper > div > div > div:first-of-type a');
 const hubspotIframeWrapper = document.createElement('div');
-const hubspotIframe = document.createElement('iframe');
+const hubspotIframe = document.createElement('img');
 hubspotIframeWrapper.className = 'hubspot-iframe-wrapper';
 hubspotIframe.src = hubspotUrl.href;
 hubspotIframeWrapper.appendChild(hubspotIframe);
 hubspotUrl.parentNode.replaceChild(hubspotIframeWrapper, hubspotUrl);
 
 const mapUrl = document.querySelector('.get-in-touch-wrapper > div > div > div:last-of-type a:nth-child(1)');
-const mapIframe = document.createElement('iframe');
+const mapIframe = document.createElement('img');
 mapIframe.src = mapUrl.href;
 mapUrl.parentNode.replaceChild(mapIframe, mapUrl);
