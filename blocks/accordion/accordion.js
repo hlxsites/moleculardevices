03d1385ce7d618dbad5a5a80159502d272905a0f
Paolo Mottadelli <paolom@adobe.com>
2023-03-15 07:44:34 +0100

    accordion removing some code

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,59 +1,34 @@
 function getNav(block) {
   const ul = document.createElement('ul');
   const titles = block.querySelectorAll('div:not(:first-child) > div:first-child');
+
   [...titles].forEach((title, i) => {
-    const li = document.createElement('li');
-    if(!i) {
-      li.classList.add('active');
-    }
-    const a = document.createElement('a');
-    a.textContent = title.textContent;
-    a.setAttribute('aria-label', title.textContent);
-    li.appendChild(a);
-    ul.appendChild(li);
+    ul.insertAdjacentHTML('beforeend', `<li><a aria-label='${title.textContent}'>${title.textContent}</a></li>`);
   });
+
+  ul.querySelector('li').classList.add('active');
   return(ul);
 }
 
 export default function decorate(block) {
-  const tabList = document.createElement('div');
+  const tabList = block.querySelector('div');
   tabList.classList.add('accordion-tab-list');
-  let listViewContent;
-  const ul = getNav(block);
-  ul.classList.add('nav', 'nav-tabs');
 
-  const viewContent = document.createElement('div');
-  viewContent.classList.add('view-content');
-
-  const viewsElContainer = document.createElement('div');
-  viewsElContainer.classList.add('views-element-container');
-  viewsElContainer.appendChild(viewContent);
+  const listViewContent = tabList.querySelector('div')
+  listViewContent.classList.add('view-content');
+  listViewContent.appendChild(getNav(block));
+  listViewContent.querySelector('ul').classList.add('nav', 'nav-tabs');
 
   const tabMainContent = document.createElement('div');
   tabMainContent.classList.add('accordion-tab-main-content');
-  tabMainContent.appendChild(viewsElContainer);
 
   [...block.children].forEach((row, i) => {
-    if(!i) {
-
-      row.classList.add('views-element-container');
-      listViewContent = row.querySelector('div');
-      listViewContent.classList.add('view-content');
-
-      listViewContent.appendChild(ul);
-      tabList.appendChild(row);
-
-    } else {
-
+    if (i) {
       row.classList.add('accordion-tab-pane');
-      if(i === 1) {
-        row.classList.add('active');
-      }
-      viewContent.appendChild(row);
-
+      tabMainContent.appendChild(row);
     }
   });
 
-  block.appendChild(tabList);
+  tabMainContent.querySelector('.accordion-tab-pane').classList.add('active');
   block.appendChild(tabMainContent);
 }
