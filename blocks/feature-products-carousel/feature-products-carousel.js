a4a3898973f39e67c24c17164b6fa4165aed1582
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-03-16 21:36:25 +0100

    90 - Refactor carousel

--- blocks/feature-products-carousel/feature-products-carousel.js
+++ blocks/feature-products-carousel/feature-products-carousel.js
@@ -2,6 +2,6 @@ import createCarousel from '../carousel/carousel.js';
 import { loadCSS } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) { 
-  loadCSS('/blocks/carousel/carousel.css', () => {});
   createCarousel(block);
+  loadCSS('/blocks/carousel/carousel.css');
 }
