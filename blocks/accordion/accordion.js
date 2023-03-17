d50fd1f51ff83d419c3df82d343b556a2a01c551
Paolo Mottadelli <paolo.moz@gmail.com>
2023-03-17 09:30:02 +0100

    accordion mobile adjust toggle logic

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,4 +1,38 @@
-function getNav(block) {
+const iconPlus = 'fa-plus';
+const iconMinus = 'fa-minus';
+const classActive = 'active';
+
+function toggleItem(item, on) {
+  if (item) {
+    const icon = item.querySelector('i');
+    if (on) {
+      item.classList.add(classActive);
+      icon && icon.classList.replace(iconPlus, iconMinus);
+    } else {
+      item.classList.remove(classActive);
+      icon && icon.classList.replace(iconMinus, iconPlus);
+    }
+  }
+}
+
+function toggleNav(target, i) {
+  const block = target.closest('.accordion.block');
+  const actives = block.querySelectorAll(`.${classActive}`);
+
+  if (actives.length) {
+    [...actives].forEach((active) => {
+      const target = active.parentElement.children[i];
+      toggleItem(active, false);
+      if (active !== target) {
+        toggleItem(target, true);
+      }
+    });
+  } else {
+    toggleItem(target.closest('.accordion-tab-pane'), true);
+  }
+}
+
+function buildNav(block) {
   const ul = document.createElement('ul');
   const titles = block.querySelectorAll('div:not(:first-child) > div:first-child');
 
@@ -7,43 +41,24 @@ function getNav(block) {
     const a = document.createElement('a');
     a.textContent = title.textContent;
     a.setAttribute('aria-label', title.textContent);
-    a.addEventListener('click', () => {
-      toggleActives(block, i);
+    a.addEventListener('click', (e) => {
+      toggleNav(e.target, i);
     });
     li.appendChild(a);
     ul.appendChild(li);
   });
 
-  ul.querySelector('li').classList.add('active');
+  ul.querySelector('li').classList.add(classActive);
   return(ul);
 }
 
-function toggleActives(block, i) {
-  const actives = block.querySelectorAll('.active');
-  [...actives].forEach((active) => {
-    active.classList.remove('active');
-    let icon = active.querySelector('i');
-    if (icon) {
-      icon.classList.remove('fa-minus');
-      icon.classList.add('fa-plus');
-    }
-    active = active.parentElement.children[i];
-    active.classList.add('active');
-    icon = active.querySelector('i');
-    if (icon) {
-      icon.classList.add('fa-minus');
-    }
-  });
-}
-
 export default function decorate(block) {
+  const ul = buildNav(block);
+  ul.classList.add('nav-tabs');
+
   const tabList = block.querySelector('div');
   tabList.classList.add('accordion-tab-list');
-
-  const listViewContent = tabList.querySelector('div')
-  listViewContent.classList.add('view-content');
-  listViewContent.appendChild(getNav(block));
-  listViewContent.querySelector('ul').classList.add('nav', 'nav-tabs');
+  tabList.querySelector('div').appendChild(ul);
 
   const tabMainContent = document.createElement('div');
   tabMainContent.classList.add('accordion-tab-main-content');
@@ -58,15 +73,15 @@ export default function decorate(block) {
 
       row.classList.add('accordion-tab-pane-inside');
       // TODO remove fix width
-      row.style.width = '1168px';
+      // row.style.width = '1168px';
       tabPane.appendChild(row);
 
       let div = row.querySelector('div');
       const button = document.createElement('button');
       button.classList.add('accordion-tab-btn');
-      button.innerHTML = `<i class='fa fa-plus'></i>${div.textContent}`;
-      button.addEventListener('click', () => {
-        toggleActives(block, i - 1);
+      button.innerHTML = `<i class='fa ${iconPlus}'></i>${div.textContent}`;
+      button.addEventListener('click', (e) => {
+        toggleNav(e.target, i - 1);
       });
       div.remove();
       row.prepend(button);
@@ -77,10 +92,10 @@ export default function decorate(block) {
   });
 
   const firstTabPane = tabMainContent.querySelector('.accordion-tab-pane');
-  firstTabPane.classList.add('active');
+  firstTabPane.classList.add(classActive);
   const firstI = firstTabPane.querySelector('i');
-  firstI.classList.remove('fa-plus');
-  firstI.classList.add('fa-minus');
+  firstI.classList.remove(iconPlus);
+  firstI.classList.add(iconMinus);
 
   block.appendChild(tabMainContent);
 }
