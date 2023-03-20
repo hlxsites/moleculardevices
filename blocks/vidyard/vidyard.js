977da7dd5739adf7d81007ec54b18ba72cbabeed
Markus Haack <mhaack@adobe.com>
2023-03-20 16:36:11 +0100

    fix: change to video URL and test thumbnail

--- blocks/vidyard/vidyard.js
+++ blocks/vidyard/vidyard.js
@@ -1,9 +1,9 @@
 import { loadScript } from '../../scripts/scripts.js';
 
-const getVidYardImg = (id, type = 'lightbox') => `<img
+const getVidYardImg = (id, thumbnail, type = 'lightbox') => `<img
     style="width: 100%; margin: auto; display: block;"
     class="vidyard-player-embed"
-    src="https://play.vidyard.com/${id}.jpg"
+    src="${thumbnail ? thumbnail : 'https://play.vidyard.com/' + id + '.jpg'}"
     data-uuid="${id}"
     data-v="4"
     data-type="${type}"
@@ -11,7 +11,14 @@ const getVidYardImg = (id, type = 'lightbox') => `<img
 
 export default function decorate(block) {
   loadScript('https://play.vidyard.com/embed/v4.js');
-  const videoId = block.textContent.trim();
 
-  block.innerHTML = getVidYardImg(videoId, block.classList.contains('lightbox') ? 'lightbox' : 'inline');
+  const videoUrl = block.querySelector('a').href;
+  const videoId = videoUrl.substring(videoUrl.lastIndexOf('/') + 1);
+  const thumbnail = block.querySelector('img');
+
+  block.innerHTML = getVidYardImg(
+    videoId,
+    thumbnail ? thumbnail.src : null,
+    block.classList.contains('lightbox') ? 'lightbox' : 'inline',
+  );
 }
