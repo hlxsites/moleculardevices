31a55ff335a7cb3c8bfda86aa32cf4ea687eb31e
José Correia <37300462+jose-correia@users.noreply.github.com>
2023-03-22 19:50:58 +0100

    Draft: Improve header loading (#106)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -102,13 +102,13 @@ export function addFavIcon(href, rel = 'icon') {
  */
 async function loadLazy(doc) {
   const main = doc.querySelector('main');
+  loadHeader(doc.querySelector('header'));
   await loadBlocks(main);
 
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
   if (hash && element) element.scrollIntoView();
 
-  loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
