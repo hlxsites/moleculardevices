cd3ce244aac7108c828413b8127cf37f2f9f6129
josec <josec@adobe.com>
2023-03-22 19:49:06 +0100

    Revert "Test async block loading"
    This reverts commit 55bca3150d7115e4fc3a46c4d91e209893f502a0.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -102,10 +102,8 @@ export function addFavIcon(href, rel = 'icon') {
  */
 async function loadLazy(doc) {
   const main = doc.querySelector('main');
-
-  const blocksPromise = loadBlocks(main);
   loadHeader(doc.querySelector('header'));
-  await blocksPromise;
+  await loadBlocks(main);
 
   const { hash } = window.location;
   const element = hash ? doc.getElementById(hash.substring(1)) : false;
