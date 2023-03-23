2771e37d8ff12f65d7f78aae65f22977f04d0d61
kapildhiman <kapil.php@gmail.com>
2023-03-23 17:19:06 +0530

    no result check

--- blocks/local-distributor/local-distributor.js
+++ blocks/local-distributor/local-distributor.js
@@ -88,7 +88,7 @@ export default async function decorate(block) {
       });
       const molAddress = `${newStr.replace(/\n/g, '<br>')}<br>`;
 
-      if ((row.PrimaryProducts.length && row.Address.length) === 0) {
+      if ((row.PrimaryProducts.length && row.Address.trim().length) === 0) {
         resultHeading.textContent = 'NO RESULT FOUND';
       } else {
         resultHeading.textContent = row.Country;
