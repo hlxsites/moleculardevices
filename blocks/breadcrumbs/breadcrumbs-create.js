3ad5b94840087399b0243c10b336f89492f005e8
Paolo Mottadelli <paolo.moz@gmail.com>
2023-03-24 17:06:27 +0100

    highlight current

--- blocks/breadcrumbs/breadcrumbs-create.js
+++ blocks/breadcrumbs/breadcrumbs-create.js
@@ -11,29 +11,28 @@ export default async function createBreadcrumbs(container) {
     .all();
   const urlForIndex = (index) => prependSlash(window.location.pathname.split('/').slice(1, index + 2).join('/'));
   const breadcrumbs = [
-    // Home link
     {
       name: 'Home',
       url_path: '/',
     },
-    // Sub-page links
     ...window.location.pathname.split('/').slice(1, -1).map((part, index) => ({
       name: newsItems.find((page) => page.path === urlForIndex(index))?.title ?? part,
-      // name: 'Home',
-      // url_path: urlForIndex(index),
-      url_path: '/',
+      url_path: urlForIndex(index),
     })),
-    // Current page
     { name: document.title },
   ];
 
   const ol = document.createElement('ol');
   breadcrumbs.forEach((crumb) => {
     const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.textContent = crumb.name;
-    a.href = crumb.url_path;
-    li.appendChild(a);
+    if (crumb.url_path) {
+      const a = document.createElement('a');
+      a.textContent = crumb.name;
+      a.href = crumb.url_path;
+      li.appendChild(a);
+    } else {
+      li.textContent = crumb.name;
+    }
     ol.appendChild(li);
   });
   container.appendChild(ol);
