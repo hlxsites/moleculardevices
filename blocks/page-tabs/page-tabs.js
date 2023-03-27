867dd6883fbbdd68d935f4de571a9ccdacbd95cd
Markus Haack <mhaack@adobe.com>
2023-03-27 11:50:46 +0200

    fix: only decorate if we have names sections

--- blocks/page-tabs/page-tabs.js
+++ blocks/page-tabs/page-tabs.js
@@ -41,16 +41,18 @@ export default function decorate(block) {
   const main = block.closest('main');
   const sections = main.querySelectorAll('div.section.tabs');
   const namedSections = [...sections].filter((section) => section.hasAttribute('data-name'));
-  const activeHash = window.location.hash;
-  const active = activeHash ? activeHash.substring(1, activeHash.length) : namedSections[0].id;
+  if (namedSections) {
+    const activeHash = window.location.hash;
+    const active = activeHash ? activeHash.substring(1, activeHash.length) : namedSections[0].id;
 
-  sections.forEach((section) => {
-    if (active === section.getAttribute('aria-labelledby')) {
-      section.setAttribute('aria-hidden', false);
-    } else {
-      section.setAttribute('aria-hidden', true);
-    }
-  });
+    sections.forEach((section) => {
+      if (active === section.getAttribute('aria-labelledby')) {
+        section.setAttribute('aria-hidden', false);
+      } else {
+        section.setAttribute('aria-hidden', true);
+      }
+    });
 
-  block.append(createTabList(namedSections, active));
+    block.append(createTabList(namedSections, active));
+  }
 }
