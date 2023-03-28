4081ef2fa764f94ef6fec609dd618d6cc8bce732
Paolo Mottadelli <paolom@adobe.com>
2023-03-28 11:48:47 +0200

    adjust footer styles

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,35 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
+function addEventListeners(container) {
+  const h3s = container.querySelectorAll('h3');
+  [...h3s].forEach((h3) => {
+    h3.addEventListener('click', (e) => {
+      toggleNewsEvents(container, e.target);
+    });
+  })
+}
+
+function toggleNewsEvents(container, target) {
+  if (!target.parentElement.classList.contains('on')) {
+    const items = container.querySelectorAll('.toggle');
+    [...items].forEach((item) => {
+      item.classList.toggle('on');
+    });
+  }
+}
+
+function buildNewsEvents(container) {
+  [...container.children].forEach((row, y) => {
+    [...row.children].forEach((column, x) => {
+      column.classList.add('toggle');
+      if (x === 0) {
+        column.classList.add('on');
+      }
+    });
+  });
+  addEventListeners(container);
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The header block element
@@ -19,6 +49,8 @@ export default async function decorate(block) {
   const footerBottom = document.createElement('div');
   footerWrap.classList.add('footer-wrap');
   footerBottom.classList.add('footer-bottom');
+  block.appendChild(footerWrap);
+  block.appendChild(footerBottom);
 
   [...footer.children].forEach((row, i) => {
     row.classList.add(`row-${i + 1}`);
@@ -29,8 +61,8 @@ export default async function decorate(block) {
     }
   });
 
-  block.appendChild(footerWrap);
-  block.appendChild(footerBottom);
+  const footerNewsEvents = block.querySelector('.footer-news-events');
+  buildNewsEvents(footerNewsEvents);
 
   await decorateIcons(footer);
   block.append(footer);
