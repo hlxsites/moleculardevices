1c65d162848d4e18168bb36fac61197fdf5943df
Paolo Mottadelli <paolom@adobe.com>
2023-03-28 10:08:08 +0200

    footer refactoring

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,26 +1,5 @@
 import { readBlockConfig, decorateIcons } from '../../scripts/lib-franklin.js';
 
-function switchGroup(selectionTitle, position) {
-  [...selectionTitle.parentElement.children].forEach((h3) => {
-    if (h3 !== selectionTitle) {
-      h3.classList.add('off');
-      h3.classList.remove('on');
-    } else {
-      h3.classList.add('on');
-      h3.classList.remove('off');
-    }
-  });
-  const blockDiv = selectionTitle.closest('.footer-news-events');
-  [...blockDiv.children].forEach((div) => {
-    div.classList.add('off');
-    div.classList.remove('on');
-  });
-  blockDiv.children[0].classList.add('on');
-  blockDiv.children[0].classList.remove('off');
-  blockDiv.children[position].classList.add('on');
-  blockDiv.children[position].classList.remove('off');
-}
-
 /**
  * loads and decorates the footer
  * @param {Element} block The header block element
@@ -36,27 +15,22 @@ export default async function decorate(block) {
   const footer = document.createElement('div');
   footer.innerHTML = html;
 
-  const titles = footer.querySelectorAll('.footer-news-events h3');
-  [...titles].forEach((title, i) => {
-    const a = document.createElement('a');
-    a.textContent = title.textContent;
-    a.setAttribute('aria-label', title.textContent);
-    title.textContent = '';
-    title.append(a);
-    title.querySelector('a').addEventListener('click', () => {
-      switchGroup(title, i + 1);
-    });
-  });
+  const footerWrap = document.createElement('div');
+  const footerBottom = document.createElement('div');
+  footerWrap.classList.add('footer-wrap');
+  footerBottom.classList.add('footer-bottom');
 
-  const blockDiv = footer.querySelector('.footer-news-events');
-  [...blockDiv.children].forEach((div) => {
-    const pars = div.querySelectorAll('p');
-    const par = pars.item(pars.length - 1);
-    if (par) {
-      par.innerHTML
-        += "<span class='icon-icon_link'></span>";
+  [...footer.children].forEach((row, i) => {
+    row.classList.add(`row-${i + 1}`);
+    if (i <= 3) {
+      footerWrap.appendChild(row);
+    } else {
+      footerBottom.appendChild(row);
     }
   });
+
+  block.appendChild(footerWrap);
+  block.appendChild(footerBottom);
 
   await decorateIcons(footer);
   block.append(footer);
