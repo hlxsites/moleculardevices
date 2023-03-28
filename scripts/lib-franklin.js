f71f713797686c76d9f98f1956c3ae7ce4e71e79
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-03-28 18:15:50 +0200

    121 - Features block and section

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -282,26 +282,20 @@ export function decorateSections(main) {
           const styles = meta.style.split(',').map((style) => toClassName(style.trim()));
           styles.forEach((style) => section.classList.add(style));
         } else if (key === 'background') {
-          let { background } = meta;
-          const backgroundImages = [];
-          background = Array.isArray(background) ? background : [background];
-          background.forEach((backgroundItem) => {
-            if (backgroundItem.startsWith('http')) {
-              const url = new URL(backgroundItem, window.location.href);
-              const { pathname } = url;
-              const exts = ['webply', pathname.substring(pathname.lastIndexOf('.') + 1)];
-              if (imageMediaQuery.matches) {
-                exts.forEach((ext) => backgroundImages.push(`url(${pathname}?width=2000&format=${ext}&optimize=medium)`));
-              } else {
-                exts.forEach((ext) => backgroundImages.push(`url(${pathname}?width=750&format=${ext}&optimize=medium)`));
-              }
+          const { background } = meta;
+          if (background.startsWith('http')) {
+            const url = new URL(background, window.location.href);
+            const { pathname } = url;
+            const backgroundImages = [];
+            const exts = ['webply', pathname.substring(pathname.lastIndexOf('.') + 1)];
+            if (imageMediaQuery.matches) {
+              exts.forEach((ext) => backgroundImages.push(`url(${pathname}?width=2000&format=${ext}&optimize=medium)`));
             } else {
-              section.style.background = backgroundItem;
+              exts.forEach((ext) => backgroundImages.push(`url(${pathname}?width=750&format=${ext}&optimize=medium)`));
             }
-          });
-
-          if (backgroundImages.length !== 0) {
             section.style.backgroundImage = backgroundImages.join(', ');
+          } else {
+            section.style.background = background;
           }
         } else {
           section.dataset[toCamelCase(key)] = meta[key];
