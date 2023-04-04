17e56a748cab1416a6ec114f690035c575af3035
Paolo Mottadelli <paolo.moz@gmail.com>
2023-04-04 14:00:54 +0200

    video lightbox in hero

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,16 +1,101 @@
+import { loadScript } from '../../scripts/scripts.js';
 import createBreadcrumbs from '../breadcrumbs/breadcrumbs-create.js';
 
-function isVideo(div) {
-  const col1 = div.querySelector('div');
-  if (col1.textContent === 'video') return true;
-  return false;
+function isValidUrl(string) {
+  try {
+    new URL(string);
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
 
-function buildVideo(div){
-  const col2 = div.querySelector('div:nth-child(2)');
-  const videoCode = col2.textContent;
-  // TODO validate video code
-  if (col2) div.innerHTML = `<div class='video_icon'><a onclick='fn_vidyard_${videoCode}();'></a></div>`;
+function getUrls(text) {
+  const match = text.match(/\b((https?|ftp|file):\/\/|(www|ftp)\.)[-A-Z0-9+&@#\/%?=~_|$!:,.;]*[A-Z0-9+&@#\/%=~_|$]/ig);
+  // console.error(match);
+  // if (match) console.error(match.length);
+  if (match) return match;
+  return null;
+  // return match ? match.join("\n") : null;
+}
+
+// get the first valid video url
+function getVideoUrl(text) {
+  let videoUrl = null;
+  if (getUrls(text)) {
+    // console.error(getUrls(text).length);
+    const hostnames = ['vids.moleculardevices.com', 'share.vidyard.com'];
+    [...getUrls(text)].forEach((url) => {
+      // console.error((url));
+      if (isValidUrl(url)) {
+        // console.error(url);
+        [...hostnames].forEach((hostname) => {
+          // console.error(hostname);
+          if (url.includes(hostname)) {
+            // console.error(`returning url: ${url}`)
+            videoUrl = url;
+          }
+        });   
+      }
+    });
+  }
+  // console.error('null');
+  return videoUrl;
+}
+
+// function isVideoDomain(text) {
+//   let isVideo = false;
+//   if (isValidUrl(text)) {
+//     const domains = ['vids.moleculardevices.com', 'share.vidyard.com'];
+//     [...domains].forEach((domain) => {
+//       if (text.includes(domain)) isVideo = true;
+//     })
+//   }
+//   return isVideo;
+// }
+
+function getVideoId(text) {
+  if (getVideoUrl(text)) {
+    const parts = getVideoUrl(text).split('/');
+    return parts.at(-1).trim();
+  }
+  return null;
+}
+
+// function getVideoId(text) {
+//   if (isVideoDomain(text)) {
+//     const parts = text.split('/');
+//     return parts.at(-1).trim();
+//   }
+//   return null;
+// }
+
+function buildVideo(block, div, videoId) {
+  const observer = new IntersectionObserver((entries) => {
+    if (entries.some((e) => e.isIntersecting)) {
+      observer.disconnect();
+      loadScript('https://play.vidyard.com/embed/v4.js');
+
+      const videoIcon = document.createElement('div');
+      const thumbnail = document.createElement('img');
+      videoIcon.append(thumbnail);
+
+      videoIcon.classList.add('video_icon');
+      thumbnail.src = '/images/play_icon.png';
+
+      div.innerHTML = `<div id="sample">
+        <div class="vidyard-player-embed" data-uuid="${videoId}" data-v="4" data-type="lightbox"></div>
+      </div>`
+      div.appendChild(videoIcon);
+
+      thumbnail.addEventListener('click', () => {
+        var players = VidyardV4.api.getPlayersByUUID(videoId);
+        var player = players[0];
+        player.showLightbox();
+      });
+    }
+  });
+  observer.observe(block);
 }
 
 export default async function decorate(block) {
@@ -21,7 +106,10 @@ export default async function decorate(block) {
   }
 
   [...block.children].forEach((div) => {
-    if (isVideo(div)) buildVideo(div);
+    if (getVideoId(div.textContent)) {
+      div.classList.add('video-column');
+      buildVideo(block, div, getVideoId(div.textContent));
+    }
     container.appendChild(div);
   });
 
