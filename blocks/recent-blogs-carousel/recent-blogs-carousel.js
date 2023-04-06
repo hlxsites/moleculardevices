949d1d0eba369f6e70f1a98651977e7a4272a636
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-04-06 11:45:14 +0200

    73 - Recent Blogs

--- blocks/recent-blogs-carousel/recent-blogs-carousel.js
+++ blocks/recent-blogs-carousel/recent-blogs-carousel.js
@@ -3,21 +3,36 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 import { a, div, h3, p } from '../../scripts/dom-helpers.js';
 import createCarousel from '../carousel/carousel.js';
 
+function summariseDescription(description) {
+  let result = description;
+  if (result.length > 75) {
+    result = result.substring(0, 75);
+    const lastSpaceIndex = result.lastIndexOf(' ');
+    if (lastSpaceIndex !== -1) {
+      result = result.substring(0, lastSpaceIndex);
+    }
+  }
+  return result + '…';
+}
+
 function renderItem(item) {
+  const itemImage  = item.thumbnail && item.thumbnail !== '0' ? item.thumbnail : item.image;
+  const buttonText = item.cardC2A && item.cardC2A !== '0' ? item.cardC2A : 'Read More';
+
   return (
     div({ class: 'blog-carousel-item' },
       div({ class: 'blog-carousel-thumb' },
         a({ href: item.path },
-          createOptimizedPicture(item.image, item.title, 'lazy', [{ width: '800' }]),
+          createOptimizedPicture(itemImage, item.title, 'lazy', [{ width: '800' }]),
         )
       ),
       div({ class: 'blog-carousel-caption' },
         a({ href: item.path },
           h3(item.title),
         ),
-        p({ class: 'blog-description' }, item.description),
+        p({ class: 'blog-description' }, summariseDescription(item.description)),
         p({ class: 'button-container' },
-          a({ href: item.path, 'aria-label': 'Read More', class: 'button primary' }, 'Read More'),
+          a({ href: item.path, 'aria-label': buttonText, class: 'button primary' }, buttonText),
         ),
       ),
     )
