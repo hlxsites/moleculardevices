10ebb90846cbf167a771d2c782b50696edc324fd
Duy Nguyen <dunguyen@adobe.com>
2023-04-06 17:14:10 +0200

    respect the order of assay elements in block

--- blocks/related-assay-data/related-assay-data.js
+++ blocks/related-assay-data/related-assay-data.js
@@ -1,5 +1,5 @@
 import { fetchFragment } from '../../scripts/scripts.js';
-import { div, h2 } from '../../scripts/dom-helpers.js';
+import { div, h3 } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
   const fragmentPaths = [...block.querySelectorAll('a')].map((a) => a.href);
@@ -8,36 +8,39 @@ export default async function decorate(block) {
     return '';
   }
 
-  let fragments = await Promise.all(fragmentPaths.map(async (path) => {
+  const fragments = {};
+  await Promise.all(fragmentPaths.map(async (path) => {
     const fragmentHtml = await fetchFragment(path);
     if (fragmentHtml) {
       const fragmentElement = document.createElement('div');
       fragmentElement.innerHTML = fragmentHtml;
-      const h1 = fragmentElement.querySelector('h1');
-      const { id } = h1;
-      const title = h1.textContent;
+      const headerBlock = fragmentElement.querySelector('h3');
       const pictureBlock = fragmentElement.querySelector('picture');
       const otherChildren = fragmentElement.querySelectorAll('p:not(:has(picture))');
-      return {
-        id, title, pictureBlock, otherChildren,
-      };
+      fragments[path] = {
+        headerBlock,
+        pictureBlock,
+        otherChildren
+      }
     }
-    return null;
   }));
-  fragments = fragments.filter((item) => !!item);
 
-  fragments.forEach((fragment) => {
-    block.append(
-      div({ class: 'assay' },
-        div({ class: 'assay-content' },
-          h2({ class: 'assay-title', id: fragment.id }, fragment.title),
-          ...fragment.otherChildren,
+  fragmentPaths.forEach((path) => {
+    const fragment = fragments[path];
+    if (fragment) {
+      const { headerBlock, otherChildren, pictureBlock } = fragment;
+      block.append(
+        div({ class: 'assay' },
+          div({ class: 'assay-content' },
+            headerBlock,
+            ...otherChildren,
+          ),
+          div({ class: 'assay-picture' },
+            pictureBlock,
+          ),
         ),
-        div({ class: 'assay-picture' },
-          fragment.pictureBlock,
-        ),
-      ),
-    );
+      );
+    }
   });
 
   return block;
