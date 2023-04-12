c3f620c3bb0a2b849b7f61692fb8ddf310c46297
Paolo Mottadelli <paolo.moz@gmail.com>
2023-04-12 11:55:09 +0200

    test: revert to main

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -18,7 +18,7 @@ import {
 import loadHeader from './header-utils.js';
 import TEMPLATE_LIST from '../templates/config.js';
 
-const LCP_BLOCKS = ['hero']; // add your LCP blocks to the list
+const LCP_BLOCKS = []; // add your LCP blocks to the list
 window.hlx.RUM_GENERATION = 'project-1'; // add your RUM generation information here
 
 export function loadScript(url, callback, type, async) {
@@ -136,10 +136,8 @@ async function loadEager(doc) {
   decorateTemplateAndTheme();
   const main = doc.querySelector('main');
   if (main) {
-    // await decorateTemplates(main);
-    // await decorateMain(main);
-    // await waitForLCP(LCP_BLOCKS);
-    decorateMain(main);
+    await decorateTemplates(main);
+    await decorateMain(main);
     await waitForLCP(LCP_BLOCKS);
   }
 }
