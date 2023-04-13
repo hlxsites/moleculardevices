0052efca10ec889bd564ada4894d7f515b5378a3
puric <puric@adobe.com>
2023-04-13 12:08:56 +0200

    add missing await in publication

--- blocks/publication/publication.js
+++ blocks/publication/publication.js
@@ -10,5 +10,5 @@ export default async function decorate(block) {
   const showDescription = true;
   const viewMoreText = 'learn more';
   // console.log(`found ${entries.length} entries`);
-  createOverview(block, entries, limit, paginationLimit, showDescription, viewMoreText);
+  await createOverview(block, entries, limit, paginationLimit, showDescription, viewMoreText);
 }
