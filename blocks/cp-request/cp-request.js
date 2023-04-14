be58e515f15f3557adb1b48180a0f619cbbf955b
kapildhiman <kapil.php@gmail.com>
2023-04-14 19:15:47 +0530

    lint issue fixed

--- blocks/cp-request/cp-request.js
+++ blocks/cp-request/cp-request.js
@@ -1,21 +1,21 @@
 import { getQueryParameter } from '../../scripts/scripts.js';
 
 function setCookie(cname, cvalue, exdays) {
-  let d = new Date();
+  const d = new Date();
   let cHostName = '';
   let nDomain = '';
   let expires = '';
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   if (exdays !== 0) {
-    expires = 'expires=' + d.toUTCString();
+    expires = `expires=${d.toUTCString()}`;
   }
 
   nDomain = window.location.hostname.endsWith('.moleculardevices.com');
-  if (nDomain == true) {
+  if (nDomain === true) {
     cHostName = 'domain=.moleculardevices.com;';
   }
-  //document.cookie = cname + "=" + cvalue + ";secure;" + cHostName + expires + ";path=/";
-  document.cookie = cname + '=' + cvalue + ';secure;' + expires + ';path=/';
+  document.cookie = `${cname}=${cvalue};${secure};${cHostName}${expires};path=/`;
+  //document.cookie = cname + '=' + cvalue + ';secure;' + expires + ';path=/'; 
 }
 
 const paramName = 'STYXKEY_PortalUserRole';
