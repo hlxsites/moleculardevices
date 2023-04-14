d4ba5a64d50348e1ac0416f7cd8d39ac1feeed7e
kapildhiman <kapil.php@gmail.com>
2023-04-14 19:08:38 +0530

    lint issue fixed

--- blocks/cp-request/cp-request.js
+++ blocks/cp-request/cp-request.js
@@ -1,21 +1,21 @@
 import { getQueryParameter } from '../../scripts/scripts.js';
 
 function setCookie(cname, cvalue, exdays) {
-  var d = new Date();
-  var c_host_name = '';
-  var n_domain = '';
-  var expires = '';
+  let d = new Date();
+  let cHostName = '';
+  let nDomain = '';
+  let expires = '';
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  if (exdays != 0) {
-    expires = "expires=" + d.toUTCString();
+  if (exdays !== 0) {
+    expires = 'expires=' + d.toUTCString();
   }
 
-  n_domain = window.location.hostname.endsWith(".moleculardevices.com");
-  if (n_domain == true) {
-    c_host_name = 'domain=.moleculardevices.com;';
+  nDomain = window.location.hostname.endsWith('.moleculardevices.com');
+  if (nDomain == true) {
+    cHostName = 'domain=.moleculardevices.com;';
   }
-  //document.cookie = cname + "=" + cvalue + ";secure;" + c_host_name + expires + ";path=/";
-  document.cookie = cname + "=" + cvalue + ";secure;" + expires + ";path=/";
+  //document.cookie = cname + "=" + cvalue + ";secure;" + cHostName + expires + ";path=/";
+  document.cookie = cname + '=' + cvalue + ';secure;' + expires + ';path=/';
 }
 
 const paramName = 'STYXKEY_PortalUserRole';
@@ -23,7 +23,7 @@ const readQuery = getQueryParameter();
 const queryStringParam = readQuery[paramName] ? readQuery[paramName] : '';
 
 if (queryStringParam == 'cplogout') {
-  setCookie("STYXKEY_PortalUserRole", '', -1);
+  setCookie('STYXKEY_PortalUserRole', '', -1);
 } else {
-  setCookie("STYXKEY_PortalUserRole", queryStringParam, 0);
+  setCookie('STYXKEY_PortalUserRole', queryStringParam, 0);
 }
