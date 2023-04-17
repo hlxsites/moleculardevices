d5706d85208b070b978dedf3e8c430d70e3cb1eb
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-04-17 17:09:28 +0200

    198 - Low LCP Scores for the pages in main

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,7 +16,11 @@ import {
   decorateBlock,
   buildBlock,
 } from './lib-franklin.js';
-import TEMPLATE_LIST from '../templates/config.js';
+
+/**
+ * to add/remove a template, just add/remove it in the list below
+ */
+const TEMPLATE_LIST = ['application-note', 'news', 'publication', 'blog'];
 
 const LCP_BLOCKS = ['hero']; // add your LCP blocks to the list
 window.hlx.RUM_GENERATION = 'molecular-devices'; // add your RUM generation information here
