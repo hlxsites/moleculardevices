42d3fc4fbfaae3c50b98f1a86e40598849e02424
puric <puric@adobe.com>
2023-05-11 15:14:48 +0200

    add 2 column layout, remove themes

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,45 +1,83 @@
-import { createIcon, decorateIcons } from '../../scripts/lib-franklin.js';
+import { decorateIcons } from '../../scripts/lib-franklin.js';
+import {
+  div, span,
+} from '../../scripts/dom-helpers.js';
 
 const openAttribute = 'aria-expanded';
+
+function applyColumnLayout(contentNodes) {
+  let applyLayout = false;
+  contentNodes.forEach((elem) => {
+    if (applyLayout) return;
+    if (elem.querySelector('picture')) {
+      applyLayout = true;
+    }
+  });
+  return applyLayout;
+}
+
+function renderColumnLayout(row) {
+  const picture = row[0];
+  const text = row[1];
+  const link = row[2];
+  if (link) link.querySelector('a').append(span({ class: 'icon icon-fa-arrow-circle-right' }));
+
+  const leftCol = div({ class: 'accordion-content-col-left' }, picture);
+  const rightCol = div({ class: 'accordion-content-col-right' }, text, link);
+  const rowContent = div({ class: 'accordion-content-row' }, leftCol, rightCol);
+  return rowContent;
+}
+
+async function renderContent(container, content) {
+  // prepare content
+  const rows = [];
+  content.forEach((elem) => {
+    if (elem.querySelector('picture')) {
+      rows.push([]);
+    }
+    if (rows.length - 1 < 0) rows.push([]);
+    rows[rows.length - 1].push(elem);
+  });
+
+  // render content
+  const contentDiv = div({ class: 'accordion-content' });
+  rows.forEach((row) => {
+    const hasColumnLayout = applyColumnLayout(row);
+    if (hasColumnLayout) {
+      const rowContent = renderColumnLayout(row);
+      contentDiv.appendChild(rowContent);
+    } else {
+      row.forEach((elem) => {
+        contentDiv.append(elem);
+      });
+    }
+  });
+  container.append(contentDiv);
+}
 
 export default async function decorate(block) {
   const isTypeNumbers = block.classList.contains('numbers');
   const accordionItems = block.querySelectorAll(':scope > div > div');
   accordionItems.forEach((accordionItem, idx) => {
     const nodes = accordionItem.children;
-
-    let number = '';
-    if (isTypeNumbers) {
-      number = document.createElement('span');
-      number.classList.add('number');
-      number.innerHTML = (idx + 1);
-    }
-
     const titleText = nodes[0];
     const rest = Array.prototype.slice.call(nodes, 1);
 
-    const titleDiv = document.createElement('div');
-    if (number) titleDiv.append(number);
-    titleDiv.appendChild(titleText);
-    titleDiv.appendChild(createIcon('fa-chevron-right'));
+    const header = div({ class: 'accordion-trigger' },
+      (isTypeNumbers) ? span({ class: 'number' }, (idx + 1)) : '',
+      titleText,
+      span({ class: 'icon icon-fa-chevron-right' }),
+    );
 
-    titleDiv.classList.add('accordion-trigger');
+    const item = div({ class: 'accordion-item' });
+    item.appendChild(header);
+    renderContent(item, rest);
 
-    const content = document.createElement('div');
-    content.classList.add('accordion-content');
-    rest.forEach((elem) => {
-      content.appendChild(elem);
-    });
+    if (idx === 0) item.setAttribute(openAttribute, '');
 
-    const newItem = document.createElement('div');
-    newItem.appendChild(titleDiv);
-    newItem.appendChild(content);
+    decorateIcons(item);
 
-    newItem.classList.add('accordion-item');
-    if (idx === 0) newItem.setAttribute(openAttribute, '');
-    decorateIcons(newItem);
-
-    accordionItem.replaceWith(newItem);
+    accordionItem.replaceWith(item);
   });
 
   const triggers = block.querySelectorAll('.accordion-trigger');
