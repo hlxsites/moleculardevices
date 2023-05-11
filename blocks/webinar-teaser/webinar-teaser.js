d9381d5a02120413d75de85697c23a494c7c8f7a
puric <puric@adobe.com>
2023-05-11 19:18:31 +0200

    init

---
+++ blocks/webinar-teaser/webinar-teaser.js
@@ -0,0 +1,46 @@
+import {
+  div, h2,
+} from '../../scripts/dom-helpers.js';
+
+export default function decorate(block) {
+  const root = block.querySelector(':scope > div > div');
+  const nodes = root.children;
+  const teaserTitle = nodes[0];
+  const webinarTitle = nodes[1];
+  const speaker = nodes[2];
+  const speakerTitle = nodes[3];
+  const videoSection = nodes[4];
+  const webinarDescription = nodes[5];
+  const registerButton = nodes[6];
+
+  block.innerHTML = '';
+  const header = (
+    div({ class: 'webinar-teaser-header' },
+      teaserTitle,
+      webinarTitle,
+    )
+  );
+  const content = (
+    div({ class: 'webinar-teaser-content' },
+      div({ class: 'webinar-teaser-left-col' },
+        div({ class: 'webinar-teaser-video' },
+          div({ class: 'webinar-teaser-video-header' },
+            speaker,
+            speakerTitle,
+          ),
+          div({ class: 'webinar-teaser-video-link' },
+            videoSection,
+          ),
+        ),
+      ),
+      div({ class: 'webinar-teaser-right-col' },
+        div({ class: 'teaser-form' },
+          webinarDescription,
+          registerButton,
+        ),
+      ),
+    )
+  );
+  block.appendChild(header);
+  block.appendChild(content);
+}
