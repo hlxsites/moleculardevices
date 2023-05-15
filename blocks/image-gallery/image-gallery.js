2ac64df69368ca0d6d8274a255b30ac62818fbc4
Paolo Mottadelli <paolom@adobe.com>
2023-05-15 17:29:48 +0200

    add styling

--- blocks/image-gallery/image-gallery.js
+++ blocks/image-gallery/image-gallery.js
@@ -1,54 +1,33 @@
 import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { div, span } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
-  // console.log('decorating image-gallery', block);
+  block.parentElement.classList.add('preview');
+  const overlayContent = div({ class: 'overlay-content'});
+  block.parentElement.appendChild(overlayContent);
+  overlayContent.appendChild(block);
 
-  block.classList.add('preview');
   [...block.children].forEach((row, i) => {
     const img = row.querySelector('img:first-of-type');
     img.addEventListener('click', () => {
-      block.classList.add('overlay');
-      block.classList.remove('preview');
+      block.parentElement.parentElement.classList.add('overlay');
+      block.parentElement.parentElement.classList.remove('preview');
     });
   });
-  block.prepend(span({ class: 'icon icon-close-circle-outline gallery-button-close' }));
-  block.prepend(span({ class: 'icon icon-icon_link gallery-button-right' }));
-  block.prepend(span({ class: 'icon icon-icon_link gallery-button-left' }));
 
-  const body = document.querySelector('body');
-
-  // for burron event listener example see hero-advanced.js line 31 etc..
-
-  block.querySelector('.gallery-button-left').addEventListener('click', () => {
-    const visiblePicture = block.querySelector('.gallery > div:not([aria-hidden="true"])');
-    const previousPicture = visiblePicture.previousElementSibling;
-
-    visiblePicture.setAttribute('aria-hidden', true);
-    if (previousPicture) {
-      previousPicture.removeAttribute('aria-hidden');
-    } else {
-      visiblePicture.parentNode.lastElementChild.removeAttribute('aria-hidden');
-    }
+  const close = span({ class: 'icon icon-close-circle-outline gallery-button-close' });
+  close.addEventListener('click', () => {
+    block.parentElement.parentElement.classList.remove('overlay');
+    block.parentElement.parentElement.classList.add('preview');
   });
 
-  block.querySelector('.gallery-button-right').addEventListener('click', () => {
-    const visiblePicture = block.querySelector('.gallery > div:not([aria-hidden="true"])');
-    const nextPicture = visiblePicture.nextElementSibling;
+  const right = span({ class: 'icon icon-icon_link gallery-button-right' });
+  const left = span({ class: 'icon icon-icon_link gallery-button-left' });
+  const scroll = (leftScroll) => block.scrollTo({ top: 0, left: leftScroll, behavior: 'smooth' });
+  right.addEventListener('click', () => { scroll(block.scrollLeft + block.parentElement.offsetWidth); });
+  left.addEventListener('click', () => { scroll(block.scrollLeft - block.parentElement.offsetWidth); });
 
-    visiblePicture.setAttribute('aria-hidden', true);
-    if (nextPicture) {
-      nextPicture.removeAttribute('aria-hidden');
-    } else {
-      visiblePicture.parentNode.firstElementChild.removeAttribute('aria-hidden');
-    }
-  });
+  block.parentElement.append(close, right, left);
 
-  block.querySelector('.gallery-button-close').addEventListener('click', () => {
-    body.classList.remove('no-scroll');
-    block.setAttribute('aria-hidden', true);
-    const visiblePicture = block.querySelector('.gallery > div:not([aria-hidden="true"])');
-    visiblePicture.setAttribute('aria-hidden', true);
-  });
-
-  await decorateIcons(block);
+  await decorateIcons(block.parentElement);
 }
