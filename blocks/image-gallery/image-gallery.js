78b9506046577cf93f9bb98645f51b39c20c0410
Paolo Mottadelli <paolo.moz@gmail.com>
2023-05-15 18:29:44 +0200

    preview styling and scroll to selected

--- blocks/image-gallery/image-gallery.js
+++ blocks/image-gallery/image-gallery.js
@@ -2,32 +2,34 @@ import { decorateIcons } from '../../scripts/lib-franklin.js';
 import { div, span } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
-  block.parentElement.classList.add('preview');
+  const wrapper = block.parentElement;
+  wrapper.classList.add('preview');
   const overlayContent = div({ class: 'overlay-content'});
-  block.parentElement.appendChild(overlayContent);
+  wrapper.appendChild(overlayContent);
   overlayContent.appendChild(block);
+
+  const scroll = (leftScroll) => block.scrollTo({ top: 0, left: leftScroll, behavior: 'smooth' });
+  const right = span({ class: 'icon icon-icon_link gallery-button-right' });
+  const left = span({ class: 'icon icon-icon_link gallery-button-left' });
+  right.addEventListener('click', () => { scroll(block.scrollLeft + overlayContent.offsetWidth); });
+  left.addEventListener('click', () => { scroll(block.scrollLeft - overlayContent.offsetWidth); });
 
   [...block.children].forEach((row, i) => {
     const img = row.querySelector('img:first-of-type');
-    img.addEventListener('click', () => {
-      block.parentElement.parentElement.classList.add('overlay');
-      block.parentElement.parentElement.classList.remove('preview');
+    img.addEventListener('click', (e) => {
+      wrapper.classList.add('overlay');
+      wrapper.classList.remove('preview');
+      scroll((overlayContent.offsetWidth * (i)));
     });
   });
 
   const close = span({ class: 'icon icon-close-circle-outline gallery-button-close' });
   close.addEventListener('click', () => {
-    block.parentElement.parentElement.classList.remove('overlay');
-    block.parentElement.parentElement.classList.add('preview');
+    wrapper.classList.remove('overlay');
+    wrapper.classList.add('preview');
   });
 
-  const right = span({ class: 'icon icon-icon_link gallery-button-right' });
-  const left = span({ class: 'icon icon-icon_link gallery-button-left' });
-  const scroll = (leftScroll) => block.scrollTo({ top: 0, left: leftScroll, behavior: 'smooth' });
-  right.addEventListener('click', () => { scroll(block.scrollLeft + block.parentElement.offsetWidth); });
-  left.addEventListener('click', () => { scroll(block.scrollLeft - block.parentElement.offsetWidth); });
-
-  block.parentElement.append(close, right, left);
+  overlayContent.append(close, right, left);
 
   await decorateIcons(block.parentElement);
 }
