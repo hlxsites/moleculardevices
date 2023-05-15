9b037f50ff886843fe1b49ead99be96dea0a9a9f
Andrei Tuicu <andrei.tuicu@gmail.com>
2023-05-15 17:58:50 +0200

    tmp - disable drift

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,33 +4,32 @@ import { sampleRUM } from './lib-franklin.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-/* eslint-disable */
-function LoadDriftWidget() {
-    var t = window.driftt = window.drift = window.driftt || [];
-    if (!t.init) {
-        if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-        t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
-        t.factory = function(e) {
-        return function() {
-            var n = Array.prototype.slice.call(arguments);
-            return n.unshift(e), t.push(n), t;
-        };
-        }, t.methods.forEach(function(e) {
-        t[e] = t.factory(e);
-        }), t.load = function(t) {
-        var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-        o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-        var i = document.getElementsByTagName("script")[0];
-        i.parentNode.insertBefore(o, i);
-    };
-    }
-    drift.SNIPPET_VERSION = '0.3.1';
-    drift.load('pyupvvckemp9');
-};
-/* eslint-enable */
+// /* eslint-disable */
+// function LoadDriftWidget() {
+//     var t = window.driftt = window.drift = window.driftt || [];
+//     if (!t.init) {
+//         if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+//         t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+//         t.factory = function(e) {
+//         return function() {
+//             var n = Array.prototype.slice.call(arguments);
+//             return n.unshift(e), t.push(n), t;
+//         };
+//         }, t.methods.forEach(function(e) {
+//         t[e] = t.factory(e);
+//         }), t.load = function(t) {
+//         var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+//         o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+//         var i = document.getElementsByTagName("script")[0];
+//         i.parentNode.insertBefore(o, i);
+//     };
+//     }
+//     drift.SNIPPET_VERSION = '0.3.1';
+//     drift.load('pyupvvckemp9');
+// };
+// /* eslint-enable */
 
-/*
-if (!document.location.hostname.match('www.moleculardevices.com.cn') && !document.location.hostname.match('.hlx.page')) {
-  LoadDriftWidget();
-}
-*/
+
+// if (!document.location.hostname.match('www.moleculardevices.com.cn') && !document.location.hostname.match('.hlx.page')) {
+//   LoadDriftWidget();
+// }
