413782cba3975324bec90e376759314cb0d34ae1
Duy Nguyen <dunguyen@adobe.com>
2023-05-16 18:57:10 +0200

    fix lint errors

--- blocks/related-products/related-products.js
+++ blocks/related-products/related-products.js
@@ -1,12 +1,12 @@
-import ffetch from "../../scripts/ffetch.js";
-import { getMetadata } from "../../scripts/lib-franklin.js";
-import { createCard } from "../card/card.js";
+import ffetch from '../../scripts/ffetch.js';
+import { getMetadata } from '../../scripts/lib-franklin.js';
+import { createCard } from '../card/card.js';
 
 export default async function decorate(block) {
   const relatedProductsMeta = getMetadata('related-products');
-  const relatedProductsTitles = relatedProductsMeta.split(',').map(item => item.trim());
-  const relatedCategoriesMeta = getMetadata('related-categories');
-  const relatedCategoriesTitles = relatedCategoriesMeta.split(',').map(item => item.trim());
+  const relatedProductsTitles = relatedProductsMeta.split(',').map((item) => item.trim());
+  // const relatedCategoriesMeta = getMetadata('related-categories');
+  // const relatedCategoriesTitles = relatedCategoriesMeta.split(',').map((item) => item.trim());
 
   const products = await ffetch('/query-index.json')
     .sheet('products')
@@ -19,7 +19,7 @@ export default async function decorate(block) {
     defaultButtonText: 'Details',
   });
 
-  products.forEach(product => {
+  products.forEach((product) => {
     product.type = product.category;
     block.append(cardRenderer.renderItem(product));
   });
