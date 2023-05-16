f0a227bb5ae7b1266fee8ca8c359ad73f739d392
Paolo Mottadelli <paolo.moz@gmail.com>
2023-05-16 10:15:16 +0200

    use existing container and wrapper

--- blocks/image-gallery/image-gallery.js
+++ blocks/image-gallery/image-gallery.js
@@ -3,31 +3,19 @@ import { div, span } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
   const wrapper = block.parentElement;
-  wrapper.classList.add('preview');
-  const overlayContent = div({ class: 'overlay-content'});
-  wrapper.appendChild(overlayContent);
-  overlayContent.appendChild(block);
-
   const scroll = (leftScroll) => block.scrollTo({ top: 0, left: leftScroll, behavior: 'smooth' });
   const right = span({ class: 'icon icon-icon_link gallery-button-right' });
   const left = span({ class: 'icon icon-icon_link gallery-button-left' });
-  right.addEventListener('click', () => { scroll(block.scrollLeft + overlayContent.offsetWidth); });
-  left.addEventListener('click', () => { scroll(block.scrollLeft - overlayContent.offsetWidth); });
-
+  const close = span({ class: 'icon icon-close-circle-outline gallery-button-close' });
+  right.addEventListener('click', () => { scroll(block.scrollLeft + wrapper.offsetWidth); });
+  left.addEventListener('click', () => { scroll(block.scrollLeft - wrapper.offsetWidth); });
+  close.addEventListener('click', () => { wrapper.parentElement.classList.remove('overlay')});
+  wrapper.append(close, right, left);
   [...block.children].forEach((row, i) => {
-    const img = row.querySelector('img:first-of-type');
-    img.addEventListener('click', (e) => {
-      wrapper.classList.add('overlay');
-      wrapper.classList.remove('preview');
-      scroll((overlayContent.offsetWidth * (i)));
+    row.querySelector('img:first-of-type').addEventListener('click', (e) => {
+      wrapper.parentElement.classList.add('overlay');
+      scroll((wrapper.offsetWidth * (i)));
     });
   });
-
-  const close = span({ class: 'icon icon-close-circle-outline gallery-button-close' });
-  close.addEventListener('click', () => {
-    wrapper.classList.remove('overlay');
-    wrapper.classList.add('preview');
-  });
-  overlayContent.append(close, right, left);
-  await decorateIcons(block.parentElement);
+  await decorateIcons(wrapper);
 }
