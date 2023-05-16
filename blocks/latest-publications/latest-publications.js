a69498c2afddd0e609a0270cf83f0576b977d592
Paolo Mottadelli <paolo.moz@gmail.com>
2023-05-16 17:13:35 +0200

    latest news and publication blocks

---
+++ blocks/latest-publications/latest-publications.js
@@ -0,0 +1,10 @@
+import ffetch from '../../scripts/ffetch.js';
+import { buildList } from '../latest-news/latest-news.js';
+
+export default async function decorate(block) {
+  const data = await ffetch('/query-index.json')
+    .sheet('publications')
+    .limit(4)
+    .all();
+    buildList(data, block);
+}
