263d43e2ab8570b17428bc20f4db6bc8f1ab4487
puric <puric@adobe.com>
2023-05-22 18:24:48 +0200

    init

--- blocks/ordering-options/ordering-options.js
+++ blocks/ordering-options/ordering-options.js
@@ -1,8 +1,46 @@
+import { getCookie } from '../../scripts/scripts.js';
 import {
-  div, h3, h4, p,
+  a, div, h3, h4, i, p, span,
 } from '../../scripts/dom-helpers.js';
 
-function renderItem(item) {
+function renderAddToCart(item) {
+  if (!item) return '';
+
+  return (
+    div({ class: 'variant-item-store' },
+      div({ class: 'variant-item-store-header' },
+        p({ class: 'legend' }, `$${(item.price / 100).toLocaleString('en-US', { maximumFractionDigits: 2, minimumFractionDigits: 2 })} USD`),
+      ),
+      div({ class: 'variant-item-store-content' },
+        div({ class: 'variant-item-store-count' },
+          span({ class: 'down' }, i({ class: 'fa fa-minus', data: item.id })),
+          span({ class: 'count', id: item.id }, 1),
+          span({ class: 'up' }, i({ class: 'fa fa-plus', data: item.id })),
+        ),
+        div({ class: 'variant-item-store-add-to-cart' },
+          a({ class: 'button primary', name: 'Add to cart' }, 'Add to cart'),
+        ),
+      ),
+    )
+  );
+}
+
+function renderCart() {
+  return (
+    div({ class: 'cart-store' },
+      span({ class: 'view-cart-count' }, 2),
+      i({ class: 'fa fa-shopping-cart' }),
+      a({
+        href: 'https://shop.moleculardevices.com/cart',
+        target: '_blank',
+        name: 'View Cart',
+        rel: 'noopener noreferrer',
+      }, 'View Cart'),
+    )
+  );
+}
+
+function renderItem(item, addToCart) {
   if (!item) return '';
 
   return (
@@ -14,6 +52,7 @@ function renderItem(item) {
         ...item.variants.map((variant) => div({ class: 'variant-item' },
           h4({ class: 'legend' }, variant.public_title),
           p({ class: 'legend' }, `#${variant.sku}`),
+          (addToCart) ? renderAddToCart(variant) : '',
         ),
         ),
       ),
@@ -42,7 +81,7 @@ async function fetchOptionIntoArray(array, idx, option) {
 async function getOrderingOptions(block) {
   const refs = [...block.querySelectorAll('.ordering-options > div > div')]
     .map((ref) => (ref.innerHTML).split(', '))
-    .reduce((a, b) => a.concat(b), []);
+    .reduce((x, y) => x.concat(y), []);
 
   const options = new Array(refs.length);
   await Promise.all(refs
@@ -53,14 +92,16 @@ async function getOrderingOptions(block) {
 
 async function renderList(block) {
   const options = await getOrderingOptions(block);
+  const addToCart = (getCookie('country_code') === 'US') || true;
   const items = [];
   options.forEach((option) => {
-    items.push(renderItem(option));
+    items.push(renderItem(option, addToCart));
   });
 
   block.innerHTML = '';
   const container = div({ class: 'ordering-options-list' });
   container.append(...items);
+  if (addToCart) container.append(renderCart());
   block.append(container);
 }
 
