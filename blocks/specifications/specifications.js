b85012a7ec4c6ad2d8c57e8b8f65d1e4a5b9383c
Duy Nguyen <dunguyen@adobe.com>
2023-05-24 17:50:02 +0200

    add sticky table head

--- blocks/specifications/specifications.js
+++ blocks/specifications/specifications.js
@@ -79,7 +79,7 @@ export default async function decorate(block) {
     });
   });
 
-  const tHeadBlock = domEl('thead', { class: 'table-head' }, headRow,
+  const tHeadBlock = domEl('thead', { class: 'table-head sticky-element sticky-desktop sticky-mobile' }, headRow,
   );
   block.append(div({ class: 'table-container' },
     domEl('table', { class: 'responsive-table' }, tHeadBlock, tBodyBlock),
