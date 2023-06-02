2662a33a050f0cee956ca71029a7f43c2ba91709
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-02 18:12:13 +0200

    test: add more lines

--- blocks/page-tabs/page-tabs.js
+++ blocks/page-tabs/page-tabs.js
@@ -71,8 +71,8 @@ export default function decorate(block) {
   // decorateAnchors();
   const heroAnchor = main.querySelector('.hero-inner a[href*="#"]');
   if (heroAnchor) {
-    // const anchorId = heroAnchor.href.slice(anchor.href.indexOf('#') + 1);
-    // const heroAnchorTab = document.querySelector(`.page-tabs a[href="#${anchorId}"]`);
+    const anchorId = heroAnchor.href.slice(anchor.href.indexOf('#') + 1);
+    const heroAnchorTab = document.querySelector(`.page-tabs a[href="#${anchorId}"]`);
     // if (heroAnchorTab) {
     //   heroAnchorTab.addEventListener('click', openTab(heroAnchorTab));
     // }
