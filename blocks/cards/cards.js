eb36e86fbf4fc73ea567ebb6e8412e676f4c8572
Alex Carol <acarolcampre@adobe.com>
2023-06-02 11:57:59 +0200

    Implement cards with hover effect

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,4 +1,5 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+import { a } from '../../scripts/dom-helpers.js';
 
 /* ================ LeadershipModal Block CLASS ================ */
 class LeadershipModal {
@@ -268,6 +269,16 @@ export default function decorate(block) {
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+
+  if (block.classList.contains('image-link')) {
+    block.querySelectorAll('li').forEach((li) => {
+      const link = li.querySelector('a');
+      const picture = li.querySelector('picture');
+      const pictureClone = picture.cloneNode(true);
+      const newLink = a({ href: link.href }, pictureClone);
+      picture.parentNode.replaceChild(newLink, picture);
+    });
+  }
 
   /* ================ Leadership Block ================ */
 
