da6a47121a784bee1c65bc996c56f2e59d33e32f
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-02 18:21:13 +0200

    test: comment almost all decorate function

--- blocks/page-tabs/page-tabs.js
+++ blocks/page-tabs/page-tabs.js
@@ -52,22 +52,22 @@ function createTabList(sections, active) {
 
 export default function decorate(block) {
   const main = block.closest('main');
-  const sections = main.querySelectorAll('div.section.tabs');
-  const namedSections = [...sections].filter((section) => section.hasAttribute('data-name'));
-  if (namedSections) {
-    const activeHash = window.location.hash;
-    const active = activeHash ? activeHash.substring(1, activeHash.length) : namedSections[0].getAttribute('data-name');
+  // const sections = main.querySelectorAll('div.section.tabs');
+  // const namedSections = [...sections].filter((section) => section.hasAttribute('data-name'));
+  // if (namedSections) {
+  //   const activeHash = window.location.hash;
+  //   const active = activeHash ? activeHash.substring(1, activeHash.length) : namedSections[0].getAttribute('data-name');
 
-    sections.forEach((section) => {
-      if (active === section.getAttribute('aria-labelledby')) {
-        section.setAttribute('aria-hidden', false);
-      } else {
-        section.setAttribute('aria-hidden', true);
-      }
-    });
+  //   sections.forEach((section) => {
+  //     if (active === section.getAttribute('aria-labelledby')) {
+  //       section.setAttribute('aria-hidden', false);
+  //     } else {
+  //       section.setAttribute('aria-hidden', true);
+  //     }
+  //   });
 
-    block.append(createTabList(namedSections, active));
-  }
+  //   block.append(createTabList(namedSections, active));
+  // }
   // decorateAnchors();
   // const heroAnchor = main.querySelector('.hero-inner a[href*="#"]');
   // if (heroAnchor) {
