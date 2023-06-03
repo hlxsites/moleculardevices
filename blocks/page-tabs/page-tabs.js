bdf98a0b1708177d80d6962be9bf570c9d00331a
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-03 07:34:03 +0200

    test: back to zero

--- blocks/page-tabs/page-tabs.js
+++ blocks/page-tabs/page-tabs.js
@@ -1,19 +1,3 @@
 export default function decorate(block) {
-  const main = block.closest('main');
-  const sections = main.querySelectorAll('div.section.tabs');
-  const namedSections = [...sections].filter((section) => section.hasAttribute('data-name'));
-  if (namedSections) {
-    const activeHash = window.location.hash;
-    const active = activeHash ? activeHash.substring(1, activeHash.length) : namedSections[0].getAttribute('data-name');
-
-    sections.forEach((section) => {
-      if (active === section.getAttribute('aria-labelledby')) {
-        section.setAttribute('aria-hidden', false);
-      } else {
-        section.setAttribute('aria-hidden', true);
-      }
-    });
-
-    // block.append(createTabList(namedSections, active));
-  }
+  block.classList.add('test');
 }
