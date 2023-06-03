fd1ebfeafa83b703315f07a05c378baffaf70ef6
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-03 07:37:49 +0200

    test: 3 lines

--- blocks/page-tabs/page-tabs.js
+++ blocks/page-tabs/page-tabs.js
@@ -1,4 +1,5 @@
 export default function decorate(block) {
   const main = block.closest('main');
   const sections = main.querySelectorAll('div.section.tabs');
+  const namedSections = [...sections].filter((section) => section.hasAttribute('data-name'));
 }
