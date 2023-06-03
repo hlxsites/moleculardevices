44c380dba47ec3a4468a83a7441c17fd325d9132
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-03 07:15:49 +0200

    test: add decorate

--- blocks/page-tabs/page-tabs.js
+++ blocks/page-tabs/page-tabs.js
@@ -17,26 +17,26 @@ function openTab(target) {
 }
 
 function createTabList(sections, active) {
-  // const ul = document.createElement('ul');
-  // sections.forEach((section) => {
-  //   const li = document.createElement('li');
-  //   const a = document.createElement('a');
-  //   a.href = `#${section.getAttribute('data-name')}`;
-  //   a.id = section.getAttribute('data-name');
-  //   a.textContent = section.title;
-  //   a.addEventListener('click', (e) => {
-  //     openTab(e.target);
-  //   });
-  //   // a.addEventListener('click', openTab);
-  //   li.append(a);
-  //   if (section.getAttribute('data-name') === active) {
-  //     li.setAttribute('aria-selected', true);
-  //   } else {
-  //     li.setAttribute('aria-selected', false);
-  //   }
-  //   ul.append(li);
-  // });
-  // return ul;
+  const ul = document.createElement('ul');
+  sections.forEach((section) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.href = `#${section.getAttribute('data-name')}`;
+    a.id = section.getAttribute('data-name');
+    a.textContent = section.title;
+    a.addEventListener('click', (e) => {
+      openTab(e.target);
+    });
+    // a.addEventListener('click', openTab);
+    li.append(a);
+    if (section.getAttribute('data-name') === active) {
+      li.setAttribute('aria-selected', true);
+    } else {
+      li.setAttribute('aria-selected', false);
+    }
+    ul.append(li);
+  });
+  return ul;
 }
 
 // function decorateAnchors() {
@@ -52,22 +52,22 @@ function createTabList(sections, active) {
 
 export default function decorate(block) {
   const main = block.closest('main');
-  // const sections = main.querySelectorAll('div.section.tabs');
-  // const namedSections = [...sections].filter((section) => section.hasAttribute('data-name'));
-  // if (namedSections) {
-  //   const activeHash = window.location.hash;
-  //   const active = activeHash ? activeHash.substring(1, activeHash.length) : namedSections[0].getAttribute('data-name');
+  const sections = main.querySelectorAll('div.section.tabs');
+  const namedSections = [...sections].filter((section) => section.hasAttribute('data-name'));
+  if (namedSections) {
+    const activeHash = window.location.hash;
+    const active = activeHash ? activeHash.substring(1, activeHash.length) : namedSections[0].getAttribute('data-name');
 
-  //   sections.forEach((section) => {
-  //     if (active === section.getAttribute('aria-labelledby')) {
-  //       section.setAttribute('aria-hidden', false);
-  //     } else {
-  //       section.setAttribute('aria-hidden', true);
-  //     }
-  //   });
+    sections.forEach((section) => {
+      if (active === section.getAttribute('aria-labelledby')) {
+        section.setAttribute('aria-hidden', false);
+      } else {
+        section.setAttribute('aria-hidden', true);
+      }
+    });
 
-  //   block.append(createTabList(namedSections, active));
-  // }
+    block.append(createTabList(namedSections, active));
+  }
   // decorateAnchors();
   // const heroAnchor = main.querySelector('.hero-inner a[href*="#"]');
   // if (heroAnchor) {
