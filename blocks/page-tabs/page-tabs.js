d77acafc2e068972d0a3f72043669d3db3263839
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-03 07:39:15 +0200

    test: 6 lines

--- blocks/page-tabs/page-tabs.js
+++ blocks/page-tabs/page-tabs.js
@@ -2,4 +2,8 @@ export default function decorate(block) {
   const main = block.closest('main');
   const sections = main.querySelectorAll('div.section.tabs');
   const namedSections = [...sections].filter((section) => section.hasAttribute('data-name'));
+  if (namedSections) {
+    const activeHash = window.location.hash;
+    const active = activeHash ? activeHash.substring(1, activeHash.length) : namedSections[0].getAttribute('data-name');
+  }
 }
