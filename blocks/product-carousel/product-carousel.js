b119958821b2716fa73599bf86d16345e36f9f7d
Markus Haack <mhaack@adobe.com>
2023-06-06 18:21:04 +0200

    chore: add product carousel

---
+++ blocks/product-carousel/product-carousel.js
@@ -0,0 +1,43 @@
+import ffetch from '../../scripts/ffetch.js';
+import { createCarousel } from '../carousel/carousel.js';
+import { createCard } from '../card/card.js';
+
+export default async function decorate(block) {
+  const productPaths = [...block.querySelectorAll('a')].map((elem) => elem.getAttribute('href'));
+  const products = await ffetch('/query-index.json')
+    .sheet('products')
+    .filter((product) => productPaths.includes(product.path))
+    .all();
+
+  const cardRenderer = await createCard({
+    titleLink: false,
+    thumbnailLink: false,
+    defaultButtonText: 'Details',
+    c2aLinkStyle: true,
+  });
+
+  await createCarousel(
+    block,
+    products,
+    {
+      defaultStyling: true,
+      navButtons: window.matchMedia('only screen and (max-width: 767px)').matches || products.length > 3,
+      dotButtons: false,
+      infiniteScroll: false,
+      autoScroll: false,
+      visibleItems: [
+        {
+          items: 1,
+          condition: () => window.screen.width < 768,
+        },
+        {
+          items: 2,
+          condition: () => window.screen.width < 1200,
+        }, {
+          items: 3,
+        },
+      ],
+      cardRenderer,
+    },
+  );
+}
