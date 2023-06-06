433d708514375779be70982bd6336dc8117aaf22
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-06 06:34:22 +0200

    fix: eslint

--- templates/landing-page/landing-page.js
+++ templates/landing-page/landing-page.js
@@ -1,14 +1,13 @@
+// eslint-disable-next-line object-curly-newline
 import { img, div, h1, p } from '../../scripts/dom-helpers.js';
 
 export default function buildAutoBlocks() {
   const pageParam = (new URLSearchParams(window.location.search)).get('page');
   if (pageParam && pageParam === 'thankyou') {
     document.body.classList.add('thankyou');
-    document.querySelector('.hero > div:nth-of-type(2)').replaceWith(
-      div(
-        div(h1( 'Thank you.'), p( 'Your document is on its way.')),
-        div(img({ src: '/images/thank-you-spectra.png' }))
-      )
-    );
+    document.querySelector('.hero > div:nth-of-type(2)').replaceWith(div(
+      div(h1('Thank you.'), p('Your document is on its way.')),
+      div(img({ src: '/images/thank-you-spectra.png' })),
+    ));
   }
 }
