5adea9b27df6a0fb2a90340d0a8a029fb6d8cda4
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-07 08:31:26 +0200

    createOptimizedPicture

--- templates/landing-page/landing-page.js
+++ templates/landing-page/landing-page.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line object-curly-newline
-import { img, div, h1, p } from '../../scripts/dom-helpers.js';
-import { getMetadata } from '../../scripts/lib-franklin.js';
+import { div, h1, p } from '../../scripts/dom-helpers.js';
+import { getMetadata, createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function buildAutoBlocks() {
   const pageParam = (new URLSearchParams(window.location.search)).get('page');
@@ -8,10 +8,9 @@ export default function buildAutoBlocks() {
     document.body.classList.add('thankyou');
     document.querySelector('.hero > div:nth-of-type(2)').replaceWith(div(
       div(h1('Thank you.'), p(`Your ${getMetadata('download-title') || 'document'} is on its way.`)),
-      div(img({ src: '/images/thank-you-spectra.png' })),
+      div(createOptimizedPicture('/images/thank-you-spectra.png', 'Thank you Spectra', false, [{ width: '750' }])),
     ));
-    const columns = document.querySelectorAll('.columns');
-    [...columns].forEach((column, i) => {
+    [...document.querySelectorAll('.columns')].forEach((column, i) => {
       if (i % 2 > 0) column.classList.add('odd');
     });
   }
