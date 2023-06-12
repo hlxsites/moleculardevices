1fcd54bf6553446d59ddc9872e8ae04a37f8dbc4
José Correia <37300462+jose-correia@users.noreply.github.com>
2023-06-12 17:42:49 +0200

    Add base code for product compare feature (#584)
    ---------
    
    Co-authored-by: josec <josec@adobe.com>
    Co-authored-by: Duy Nguyen <dunguyen@adobe.com>

---
+++ templates/compare-items/compare-banner.js
@@ -0,0 +1,167 @@
+/* eslint-disable import/prefer-default-export */
+
+import { decorateIcons, loadCSS } from '../../scripts/lib-franklin.js';
+import {
+  a, div, i, span, img,
+} from '../../scripts/dom-helpers.js';
+import {
+  MAX_COMPARE_ITEMS,
+  getSelectedItems,
+  unselectAllComparedItems,
+  updateCompareButtons,
+} from '../../scripts/compare-helpers.js';
+
+class CompareBanner {
+  constructor(config = {}) {
+    this.cssFiles = [];
+    this.currentCompareItemsCount = 0;
+    this.compareButtonText = 'Compare ';
+    this.banner = document.querySelector('.compare-banner');
+
+    // Apply overwrites
+    Object.assign(this, config);
+
+    this.cssFiles.push('/templates/compare-items/compare-items.css');
+  }
+
+  render() {
+    const compareButton = a(
+      { class: 'gradiant-blue-btn btn compare-toggle' },
+      this.compareButtonText,
+      span(
+        { class: 'compare-count' },
+        this.currentCompareItemsCount,
+      ),
+      '/',
+      span(
+        { class: 'compare-total-count' },
+        MAX_COMPARE_ITEMS,
+      ),
+    );
+
+    const closeButton = a(
+      { class: 'gradiant-blue-btn btn compare-close' },
+      i(
+        { class: 'fa fa-close' },
+      ),
+    );
+
+    const compareBanner = div(
+      { class: 'compare-banner' },
+      div(
+        { class: 'container' },
+        div(
+          { class: 'compare-row' },
+          div(
+            { class: 'compare-cell' },
+            div(
+              { class: 'compare-row' },
+            ),
+          ),
+          div(
+            { class: 'compare-cell button' },
+            div(
+              { class: 'compare-data' },
+              compareButton,
+              closeButton,
+            ),
+          ),
+        ),
+      ),
+    );
+
+    compareButton.addEventListener('click', () => {
+      // TODO: open modal with comparison
+    });
+
+    closeButton.addEventListener('click', () => {
+      unselectAllComparedItems();
+      updateCompareButtons([]);
+      this.hideBanner();
+    });
+
+    decorateIcons(compareBanner);
+
+    this.banner = compareBanner;
+    return compareBanner;
+  }
+
+  showBanner() {
+    this.banner.classList.add('show');
+  }
+
+  hideBanner() {
+    this.banner.classList.remove('show');
+  }
+
+  refreshBanner() {
+    const selectedItemTitles = getSelectedItems();
+    const numSelectedItems = selectedItemTitles.length;
+    if (numSelectedItems === 0) {
+      this.hideBanner();
+      return;
+    }
+    this.banner.querySelector('.compare-count').innerHTML = numSelectedItems;
+
+    // get first compare cell
+    const compareCell = this.banner.querySelectorAll('.compare-cell')[0];
+    const cellRow = compareCell.querySelector('.compare-row');
+    cellRow.innerHTML = '';
+
+    selectedItemTitles.forEach((title) => {
+      const closeButton = a(
+        { class: 'close greenstrip_close' },
+        img(
+          { src: '/images/close.png' },
+        ),
+      );
+
+      const compareCellItem = div(
+        { class: 'compare-cell' },
+        div(
+          { class: 'compare-data', 'data-title': title },
+          title,
+          closeButton,
+        ),
+      );
+      cellRow.appendChild(compareCellItem);
+    });
+    this.showBanner();
+  }
+
+  getOrRenderBanner() {
+    let banner = document.querySelector('.compare-banner');
+    if (!banner) {
+      banner = this.render();
+
+      const main = document.querySelector('main');
+      main.append(banner);
+    }
+
+    return banner;
+  }
+
+  async loadCSSFiles() {
+    let defaultCSSPromise;
+    if (Array.isArray(this.cssFiles) && this.cssFiles.length > 0) {
+      defaultCSSPromise = new Promise((resolve) => {
+        this.cssFiles.forEach((cssFile) => {
+          loadCSS(cssFile, (e) => resolve(e));
+        });
+      });
+    }
+    // eslint-disable-next-line no-unused-expressions
+    this.cssFiles && (await defaultCSSPromise);
+  }
+}
+
+/**
+ * Create an instance of the CompareBanner class
+ * @param {Object}  config   optional - config object for
+ * customizing the rendering and behaviour
+ */
+export async function createCompareBannerInterface(config) {
+  const banner = new CompareBanner(config);
+  await banner.loadCSSFiles();
+  return banner;
+}
