c96880ad6bd2bb940d6e3ac698ef8f39187fa302
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-14 19:11:11 +0200

    fix: linting

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -5,10 +5,10 @@ export default async function decorate(block) {
   const section = block.parentElement.parentElement;
   const tocPosition = Array.from(section.children).indexOf(block.parentElement);
   const hTagNames = ['H2', 'H3', 'H4', 'H5', 'H6'];
-  const headings = section.querySelectorAll(hTagNames.map((i) => `div:nth-child(n+${tocPosition + 1}) ` + i).join(','));
+  const headings = section.querySelectorAll(hTagNames.map((i) => `div:nth-child(n+${tocPosition + 1}) ${i}`).join(','));
   const firstHTagName = headings[0].tagName;
   toc.innerHTML = '';
-  [...headings].forEach((title, i) => {
+  [...headings].forEach((title) => {
     if (hTagNames.indexOf(title.tagName) >= hTagNames.indexOf(firstHTagName)) {
       toc.append(li({ class: title.tagName }, a({ href: `#${title.id}` }, title.textContent)));
     }
