2be0b6ffb59866938a605446a432704dd12bd050
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-14 17:27:48 +0200

    toc first simplest version

---
+++ blocks/toc/toc.js
@@ -0,0 +1,11 @@
+import { li, a, h3 } from '../../scripts/dom-helpers.js';
+
+export default async function decorate(block) {
+  const toc = document.createElement('ul');
+  const section = block.parentElement.parentElement;
+  const headings = ['h2', 'h3', 'h4', 'h5', 'h6'];
+  [...section.querySelectorAll('h2, h3, h4, h5, h6')].forEach((title) => {
+    toc.append(li({ class: title.tagName }, a({ href: `#${title.id}` }, title.textContent)));
+  });
+  block.appendChild(toc);
+}
