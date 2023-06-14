6b260d81abbb1f3e25dcc1d5aac4a612d6484cc1
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-14 19:01:10 +0200

    fix: linting

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -1,17 +1,16 @@
 import { li, a } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
-  let toc = block.querySelector('ul, ol') || document.createElement('ul');
+  const toc = block.querySelector('ul, ol') || document.createElement('ul');
   const section = block.parentElement.parentElement;
   const tocPosition = Array.from(section.children).indexOf(block.parentElement);
-  let hTagNames = ['H2', 'H3', 'H4', 'H5', 'H6'];
-  const headings = section.querySelectorAll(hTagNames.map(i => `div:nth-child(n+${tocPosition + 1}) ` + i).join(','));
-  let firstHTagName = headings[0].tagName;
-  
+  const hTagNames = ['H2', 'H3', 'H4', 'H5', 'H6'];
+  const headings = section.querySelectorAll(hTagNames.map(i => (`div:nth-child(n+${tocPosition + 1}) ${i}`)).join(','));
+  const firstHTagName = headings[0].tagName;
   toc.innerHTML = '';
-  [...headings].forEach((title, i) => {
+  [...headings].forEach((title) => {
     if (hTagNames.indexOf(title.tagName) >= hTagNames.indexOf(firstHTagName)) {
-      toc.append(li({ class: title.tagName }, a({ href: `#${title.id}` }, title.textContent)));  
+      toc.append(li({ class: title.tagName }, a({ href: `#${title.id}` }, title.textContent)));
     }
   });
   block.appendChild(toc);
