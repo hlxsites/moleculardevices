c93d441aa5f17ba8ebfa5b596ce243bfe76c3766
Alex Carol <acarolcampre@adobe.com>
2023-06-14 15:22:58 +0200

    WIP

--- blocks/spectra-bot/spectra-bot.js
+++ blocks/spectra-bot/spectra-bot.js
@@ -12,6 +12,24 @@ export default function decorate(block) {
 
   const bubbleWrapper = div({ class: 'bubble-wrapper', style: `background-image: url(${bubbleSrc})` }, textDiv);
 
+  block.classList.forEach((className) => {
+    if (className.startsWith('width-')) {
+      const width = className.substring(6);
+      bgImg.style.width = `${width}px`;
+    }
+
+    if (className.startsWith('bubble-height-')) {
+      const height = className.substring(14);
+      bubbleWrapper.style.height = `${height}px`;
+    }
+
+    // TODO discriminate mobile vs desktop
+    if (className.startsWith('desktop-font-size-')) {
+      const fontSize = className.substring(18);
+      textDiv.style.fontSize = `${fontSize}px`;
+    }
+  });
+
   block.innerHTML = '';
 
   // Append the elements in the new order, adding new classes as necessary
