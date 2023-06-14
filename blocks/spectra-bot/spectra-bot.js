9d2e7cb870544f1058be95770a0ec0d6016ba495
Alex Carol <acarolcampre@adobe.com>
2023-06-14 18:54:04 +0200

    Fix multimode + force new format

--- blocks/spectra-bot/spectra-bot.js
+++ blocks/spectra-bot/spectra-bot.js
@@ -19,29 +19,9 @@ export default function decorate(block) {
     decorateBubbleWrapper(block.querySelector('div:nth-child(2)'));
     const bgImg = block.querySelector('div:nth-child(3)');
     bgImg.classList.add('background-img');
-  } else if (block.classList.contains('microplate')) {
+  } else {
     decorateBubbleWrapper(block.querySelector('div:nth-child(1)'));
     const bgImg = block.querySelector('div:nth-child(2)');
     bgImg.classList.add('background-img');
-  } else {
-    const textDiv = block.querySelector('div:nth-child(1) > div > div');
-    textDiv.classList.add('text-over');
-
-    const fgImg = block.querySelector('div:nth-child(2)');
-    const bubbleSrc = fgImg.querySelector('img').src;
-
-    const bgImg = block.querySelector('div:nth-child(3)');
-    bgImg.classList.add('background-img');
-
-    const bubbleWrapper = div({
-      class: 'bubble-wrapper',
-      style: `background-image: url(${bubbleSrc})`,
-    }, textDiv);
-
-    block.innerHTML = '';
-
-    // Append the elements in the new order, adding new classes as necessary
-    block.appendChild(bubbleWrapper);
-    block.appendChild(bgImg);
   }
 }
