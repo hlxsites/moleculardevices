691d2e533b0d2293a76af03abf8a2553e72e30df
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-14 17:30:53 +0200

    use ul or ol based on toc configuratiob

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -1,7 +1,9 @@
 import { li, a, h3 } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
-  const toc = document.createElement('ul');
+  let toc = block.querySelector('ul, ol');
+  toc.innerHTML = '';
+  if (!toc) toc = document.createElement('ul');
   const section = block.parentElement.parentElement;
   const headings = ['h2', 'h3', 'h4', 'h5', 'h6'];
   [...section.querySelectorAll('h2, h3, h4, h5, h6')].forEach((title) => {
