50e9bf7a1c4c4a7ad50646a414efc9306f4faaa9
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-14 18:55:06 +0200

    only list following and smaller headings after toc

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -1,13 +1,18 @@
-import { li, a, h3 } from '../../scripts/dom-helpers.js';
+import { li, a } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
-  let toc = block.querySelector('ul, ol');
-  toc.innerHTML = '';
-  if (!toc) toc = document.createElement('ul');
+  let toc = block.querySelector('ul, ol') || document.createElement('ul');
   const section = block.parentElement.parentElement;
-  const headings = ['h2', 'h3', 'h4', 'h5', 'h6'];
-  [...section.querySelectorAll('h2, h3, h4, h5, h6')].forEach((title) => {
-    toc.append(li({ class: title.tagName }, a({ href: `#${title.id}` }, title.textContent)));
+  const tocPosition = Array.from(section.children).indexOf(block.parentElement);
+  let hTagNames = ['H2', 'H3', 'H4', 'H5', 'H6'];
+  const headings = section.querySelectorAll(hTagNames.map(i => `div:nth-child(n+${tocPosition + 1}) ` + i).join(','));
+  let firstHTagName = headings[0].tagName;
+  
+  toc.innerHTML = '';
+  [...headings].forEach((title, i) => {
+    if (hTagNames.indexOf(title.tagName) >= hTagNames.indexOf(firstHTagName)) {
+      toc.append(li({ class: title.tagName }, a({ href: `#${title.id}` }, title.textContent)));  
+    }
   });
   block.appendChild(toc);
 }
