c0c08ea3a598962ec8e647efda8ee768b08594d9
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-15 11:13:56 +0200

    code improvements

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -1,13 +1,12 @@
 import { li, a } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
-  const toc = block.querySelector('ul, ol') || document.createElement('ul');
-  const section = block.parentElement.parentElement;
+  const toc = block.classList.contains('numbers') ? document.createElement('ol') : document.createElement('ul');
+  const section = block.closest('.section');
   const tocPosition = Array.from(section.children).indexOf(block.parentElement);
-  const hTagNames = ['H2', 'H3', 'H4', 'H5', 'H6'];
+  const hTagNames = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
   const headings = section.querySelectorAll(hTagNames.map((i) => `div:nth-child(n+${tocPosition + 1}) ${i}`).join(','));
   const firstHTagName = headings[0].tagName;
-  toc.innerHTML = '';
   [...headings].forEach((title) => {
     if (hTagNames.indexOf(title.tagName) >= hTagNames.indexOf(firstHTagName)) {
       toc.append(li({ class: title.tagName.toLowerCase() }, a({ href: `#${title.id}` }, title.textContent)));
