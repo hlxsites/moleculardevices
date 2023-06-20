f2af0b469e1c096509caa72bf90cade48bf867dc
Paolo Mottadelli <paolo.moz@gmail.com>
2023-06-20 07:59:35 +0200

    specify how many levels below in toc

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -1,6 +1,7 @@
 import { li, a } from '../../scripts/dom-helpers.js';
 
 export default async function decorate(block) {
+  const levels = /levels-(\d+)/.exec(block.className)[1];
   const toc = block.classList.contains('numbers') ? document.createElement('ol') : document.createElement('ul');
   const section = block.closest('.section');
   const tocPosition = Array.from(section.children).indexOf(block.parentElement);
@@ -8,7 +9,7 @@ export default async function decorate(block) {
   const headings = section.querySelectorAll(hTagNames.map((i) => `div:nth-child(n+${tocPosition + 1}) ${i}`).join(','));
   const firstHTagName = headings[0].tagName;
   [...headings].forEach((title) => {
-    if (hTagNames.indexOf(title.tagName) >= hTagNames.indexOf(firstHTagName)) {
+    if (hTagNames.indexOf(title.tagName) >= hTagNames.indexOf(firstHTagName) && hTagNames.indexOf(title.tagName) < (hTagNames.indexOf(firstHTagName) + Number(levels))) {
       toc.append(li({ class: title.tagName.toLowerCase() }, a({ href: `#${title.id}` }, title.textContent.replace(/^\d*[0-9].\s/g, ''))));
     }
   });
