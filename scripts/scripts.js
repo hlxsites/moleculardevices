2e4cf6a32e0d992361498014c3f5ee0e5508fd01
Markus Haack <mhaack@adobe.com>
2023-07-04 10:21:42 +0200

    fix: cmp vs. mdcmp parameter handling

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -674,7 +674,7 @@ export function getCookie(cname) {
 function setCookieFromQueryParameters(paramName, exdays) {
   const readQuery = getQueryParameter();
   if (readQuery[paramName]) {
-    setCookie(paramName, readQuery[paramName], exdays);
+    setCookie(paramName === 'mdcmp' ? 'cmp' : paramName, readQuery[paramName], exdays);
   }
 }
 
@@ -697,7 +697,7 @@ async function loadPage() {
   await loadLazy(document);
   loadDelayed();
 }
-const cookieParams = ['cmp', 'utm_medium', 'utm_source', 'utm_keyword', 'gclid'];
+const cookieParams = ['cmp', 'mdcmp', 'utm_medium', 'utm_source', 'utm_keyword', 'gclid'];
 
 cookieParams.forEach((param) => {
   setCookieFromQueryParameters(param, 0);
