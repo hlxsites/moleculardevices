23db48e5ed5416c1eefe2a684c7226ca45e6c034
Duy Nguyen <duy.phnguyen@gmail.com>
2023-07-14 13:53:13 +0200

    feat: card button default text from placeholders

--- blocks/card/card.js
+++ blocks/card/card.js
@@ -2,7 +2,7 @@
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-alert */
 
-import { decorateIcons, loadCSS, createOptimizedPicture } from '../../scripts/lib-franklin.js';
+import { decorateIcons, loadCSS, createOptimizedPicture, fetchPlaceholders } from '../../scripts/lib-franklin.js';
 import { summariseDescription } from '../../scripts/scripts.js';
 import {
   a, div, h3, p, i, span,
@@ -172,6 +172,8 @@ class Card {
  * customizing the rendering and behaviour
  */
 export async function createCard(config) {
+  const placeholders = await fetchPlaceholders();
+  config.defaultButtonText = placeholders.readMore;
   const card = new Card(config);
   await card.loadCSSFiles();
   return card;
