aa15b6077349095ac6723811059bef49b5961bbb
Alex Carol <acarolcampre@adobe.com>
2023-07-26 09:31:52 +0200

    Simplify clone-screening-carousel.js

--- blocks/clone-screening-carousel/clone-screening-carousel.js
+++ blocks/clone-screening-carousel/clone-screening-carousel.js
@@ -1,12 +1,7 @@
-import { createCarousel } from '../carousel/carousel.js';
+import carouselDecorate from '../carousel/carousel.js';
 
 export default async function decorate(block) {
-  console.log('carousel screening decorate', block);
-
-  await createCarousel(block);
+  carouselDecorate(block);
   block.classList.add('carousel');
   block.parentElement.classList.add('carousel-wrapper');
-
-  // eslint-disable-next-line max-len
-  // , [...block.children], { cssFiles: ['/blocks/carousel/carousel.css', '/blocks/clone-screening-carousel/clone-screening-carousel.css'] });
 }
