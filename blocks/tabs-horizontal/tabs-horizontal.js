cb87369281a0fbf194149aca07948199502406bc
Duy Nguyen <duy.phnguyen@gmail.com>
2023-07-27 14:18:24 +0200

    feat: implement horizontal tabs block (#962)
    * feat: implement horizontal tabs block
    
    * remove link in tabs nav
    
    * fix title styles
    
    * fix max width
    
    * fix max width
    
    * modify width
    
    * move embed fragment to scripts
    
    * no export decorate links
    
    * no export again

---
+++ blocks/tabs-horizontal/tabs-horizontal.js
@@ -0,0 +1,61 @@
+import {
+  div, li, ul,
+} from '../../scripts/dom-helpers.js';
+import { processEmbedFragment } from '../../scripts/scripts.js';
+
+const classActive = 'active';
+
+function handleTabClick(e, idx) {
+  e.preventDefault();
+  const { target } = e;
+  [...target.closest('.tabs-nav').children].forEach((nav) => nav.classList.remove(classActive));
+  target.closest('.tabs-nav-item').classList.add(classActive);
+  const panes = target.closest('.tabs-horizontal').querySelectorAll('.tab-pane');
+  [...panes].forEach((pane) => pane.classList.remove(classActive));
+  panes[idx].classList.add(classActive);
+}
+
+function buildNav(block) {
+  const titles = block.querySelectorAll('.tabs-horizontal > div > div:first-child');
+  const elemWidth = Math.floor(100 / titles.length);
+  const navList = ul({ class: 'tabs-nav' });
+  [...titles].forEach((title, idx) => {
+    const tabTitle = title.textContent;
+    const listItem = li(
+      {
+        class: 'tabs-nav-item',
+        style: `width: ${elemWidth}%;`,
+        onclick: (e) => { handleTabClick(e, idx); },
+        'aria-label': tabTitle,
+      },
+      tabTitle,
+    );
+    navList.append(listItem);
+  });
+  navList.querySelector('li').classList.add(classActive);
+  return navList;
+}
+
+async function buildTabs(block) {
+  const tabPanes = block.querySelectorAll('.tabs-horizontal > div > div:last-child');
+  const tabList = div({ class: 'tabs-list' });
+  const decoratedPanes = await Promise.all([...tabPanes].map(async (pane) => {
+    pane.classList.add('tab-pane');
+    const decoratedPane = await processEmbedFragment(pane);
+    return decoratedPane;
+  }));
+  decoratedPanes.forEach((pane) => { tabList.append(pane); });
+  tabList.querySelector('.tab-pane').classList.add(classActive);
+  return tabList;
+}
+
+export default async function decorate(block) {
+  const nav = buildNav(block);
+  const tabs = await buildTabs(block);
+  block.innerHTML = '';
+
+  block.append(nav);
+  block.append(tabs);
+
+  return block;
+}
