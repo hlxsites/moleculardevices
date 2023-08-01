5944812cac80a7f9acdd7394032c967fc6a5d1d2
Markus Haack <mhaack@adobe.com>
2023-08-01 11:01:47 +0200

    fix: landing page styles

--- templates/landing-page/landing-page.js
+++ templates/landing-page/landing-page.js
@@ -49,9 +49,6 @@ export default function buildAutoBlocks() {
       div(h1('Thank you.'), p(`Your ${getMetadata('download-title') || 'document'} is on its way.`)),
       div(createOptimizedPicture('/images/thank-you-spectra.png', 'Thank you Spectra', false, [{ width: '750' }])),
     ));
-    [...document.querySelectorAll('.columns')].forEach((column, i) => {
-      if (i % 2 > 0) column.classList.add('odd');
-    });
   }
   handleEmbed();
 }
