17e98ab5fa60c7f4414e79b66846d6316bc9ee06
Rajneesh <rajneesh.kumar@nvish.com>
2023-08-14 19:20:00 +0530

    lint issues fixed

--- blocks/additional-resources/additional-resources.js
+++ blocks/additional-resources/additional-resources.js
@@ -1,48 +1,49 @@
 import { fetchFragment } from '../../scripts/scripts.js';
 
 async function renderFragment(fragment, block, className) {
-	fragment.classList.add(className);
-	const actionLink = fragment.querySelector('div > p:last-child:last-of-type a:only-child');
-	if (actionLink) {
-		actionLink.classList.add('button', 'primary');
-	}
-	block.append(fragment);
+  fragment.classList.add(className);
+  const actionLink = fragment.querySelector('div > p:last-child:last-of-type a:only-child');
+  if (actionLink) {
+    actionLink.classList.add('button', 'primary');
+  }
+  block.append(fragment);
 }
 
 export default async function decorate(block) {
-	const fragmentPaths = [...block.querySelectorAll('a')].map((a) => a.href);
-	block.innerHTML = '';
+  const fragmentPaths = [...block.querySelectorAll('a')].map((a) => a.href);
+  block.innerHTML = '';
 
-	if (fragmentPaths.length === 0) {
-		return '';
-	}
+  if (fragmentPaths.length === 0) {
+    return '';
+  }
 
-	const fragments = await Promise.all(
-		fragmentPaths.map(async (path) => {
-			const fragmentHtml = await fetchFragment(path);
-			if (fragmentHtml) {
-				const fragmentElement = document.createElement('div');
-				fragmentElement.innerHTML = fragmentHtml;
-				const img = fragmentElement.querySelector('picture');
-				const imgAnchor = img.nextElementSibling;
-				if (imgAnchor) {
-					imgAnchor.innerHTML = '';
-					imgAnchor.appendChild(img);
-				}
-				const h3 = fragmentElement.querySelector('h3');
-				return { id: h3.id, title: h3.textContent, html: fragmentElement };
-			}
-			return null;
-		}),
-	);
+  const fragments = await Promise.all(
+    fragmentPaths.map(async (path) => {
+      const fragmentHtml = await fetchFragment(path);
+      if (fragmentHtml) {
+        const fragmentElement = document.createElement('div');
+        fragmentElement.innerHTML = fragmentHtml;
+        const img = fragmentElement.querySelector('picture');
+        const imgAnchor = img.nextElementSibling;
+        if (imgAnchor) {
+          imgAnchor.innerHTML = '';
+          imgAnchor.appendChild(img);
+        }
+        const h3 = fragmentElement.querySelector('h3');
+        return { id: h3.id, title: h3.textContent, html: fragmentElement };
+      }
+      return null;
+    }),
+  );
 
-	const apps = document.createElement('div');
-	apps.classList.add('additional-resources-container');
+  const apps = document.createElement('div');
+  apps.classList.add('additional-resources-container');
 
-	fragments.forEach((fragment) => {
-		renderFragment(fragment.html, apps, 'additional-resource');
-	});
+  fragments.forEach((fragment) => {
+    renderFragment(fragment.html, apps, 'additional-resource');
+  });
 
-	block.append(apps);
-	return block; 
+  block.append(apps);
+
+  return block;
 }
