b21c1ac6060e215fdeaa99c7ce4b20aa051a5777
Alex Carol <acarolcampre@adobe.com>
2023-08-15 10:41:00 +0200

    Initial scroll working

--- blocks/image-gallery/image-gallery.js
+++ blocks/image-gallery/image-gallery.js
@@ -7,39 +7,58 @@ export default async function decorate(block) {
 
   const lightboxOverlay = div({ class: 'image-gallery-lightbox-overlay', 'aria-hidden': true });
   body.append(lightboxOverlay);
+  const actualLightboxOverlay = document.querySelector('.image-gallery-lightbox-overlay');
 
-  block.querySelectorAll(':scope > div > div > p:nth-of-type(2) > picture').forEach((picture) => {
-    picture.parentElement.previousElementSibling.classList.add('thumbnail');
-  });
+  // block.querySelectorAll(':scope > div > div > p:nth-of-type(2) > picture').forEach((picture) => {
+  //   picture.parentElement.previousElementSibling.classList.add('thumbnail');
+  // });
 
+  const childrenLength = block.children.length;
   const scroll = (leftScroll) => {
     let resultingLeftScroll = leftScroll;
     if (leftScroll < 0) {
-      resultingLeftScroll = leftScroll + wrapper.offsetWidth * block.children.length;
-    } else if (leftScroll + wrapper.offsetWidth > wrapper.offsetWidth * block.children.length) {
+      resultingLeftScroll = leftScroll + lightboxOverlay.offsetWidth * childrenLength;
+    } else if (leftScroll + lightboxOverlay.offsetWidth > lightboxOverlay.offsetWidth * childrenLength) {
       resultingLeftScroll = 0;
     }
-    block.scrollTo({ top: 0, left: resultingLeftScroll, behavior: 'smooth' });
+    lightboxOverlay.scrollTo({ top: 0, left: resultingLeftScroll, behavior: 'smooth' });
   };
   const right = span({ class: 'icon icon-icon_link gallery-button-right' });
   const left = span({ class: 'icon icon-icon_link gallery-button-left' });
   const close = span({ class: 'icon icon-close-circle-outline gallery-button-close' });
 
-  right.addEventListener('click', () => { scroll(block.scrollLeft + wrapper.offsetWidth); });
-  left.addEventListener('click', () => { scroll(block.scrollLeft - wrapper.offsetWidth); });
+  right.addEventListener('click', () => { scroll(lightboxOverlay.scrollLeft + lightboxOverlay.offsetWidth); });
+  left.addEventListener('click', () => { scroll(actualLightboxOverlay.scrollLeft - actualLightboxOverlay.offsetWidth); });
   close.addEventListener('click', () => {
     lightboxOverlay.setAttribute('aria-hidden', true);
     body.classList.remove('no-scroll');
     // block.scrollIntoView({ behavior: 'instant', block: 'center' });
   });
 
-  lightboxOverlay.append(block, close, right, left);
+  [...block.children].forEach((row, i) => {
+    row.classList.add(`potato-${i}`);
+  });
+
+  lightboxOverlay.append(div(block.cloneNode(true), close, right, left));
+  lightboxOverlay.querySelectorAll(':scope > div > div > p.picture:nth-of-type(2)').forEach((element) => {
+    element.remove();
+  });
+  [...block.children].slice(5).forEach((row) => {
+    block.removeChild(row);
+  });
+
   [...block.children].forEach((row, i) => {
     row.querySelector('img:first-of-type').addEventListener('click', () => {
       body.classList.add('no-scroll');
       lightboxOverlay.removeAttribute('aria-hidden');
 
-      block.scrollTo({ top: 0, left: wrapper.offsetWidth * i, behavior: 'instant' });
+      const innerBlock = actualLightboxOverlay.querySelector('.block');
+
+      console.log('overlay offset width', lightboxOverlay.offsetWidth);
+      console.log('actual overlay offset width', innerBlock.offsetWidth);
+      console.log('i', i);
+      innerBlock.scrollTo({ top: 0, left: innerBlock.offsetWidth * i, behavior: 'instant' });
+      console.log('clicked');
     });
   });
   await decorateIcons(wrapper);
