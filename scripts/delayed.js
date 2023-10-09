131c0ff56ec5c5123c9419a44c91c67307fbeb04
Rajneesh <rajneesh.kumar@nvish.com>
2023-10-09 16:33:47 +0530

    added in delayed js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -100,3 +100,9 @@ if (!isSidekickLibrary) {
     LoadDriftWidget();
   }
 }
+
+(function(i,s,o,g,r,a,m){i['SLScoutObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://scout-cdn.salesloft.com/sl.js','slscout');
+    slscout(["init", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0IjoxMTA4OTF9.VTtiOYLfvqPpOmg9SD5Pl4uz7Zmn-achb0JFgYmfFs0"])
