294064173a32dce339d6f0a69beb1be1bcf7b857
Markus Haack <mhaack@adobe.com>
2023-10-10 16:30:04 +0200

    fix: lint issues

--- blocks/quote-request/quote-request.js
+++ blocks/quote-request/quote-request.js
@@ -134,7 +134,7 @@ async function loadIframeForm(data, type) {
   loadScript('../../scripts/iframeResizer.min.js');
   const formUrl = 'https://info.moleculardevices.com/rfq';
   const root = document.getElementById('step-3');
-  const rfqRUM = { source: 'global'};
+  const rfqRUM = { source: 'global' };
   root.innerHTML = '';
 
   let tab = '';
