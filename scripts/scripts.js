a632c0d530c55f08758cdb7cfa56ae80607d353d
Ekrem Doğan <ekremney@users.noreply.github.com>
2024-01-18 15:31:07 +0100

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -930,7 +930,7 @@ async function loadLazy(doc) {
   // eslint-disable-next-line no-unused-vars
   // loadHeader(doc.querySelector('header'));
 
-  await loadBlocks(main);
+  // await loadBlocks(main);
 
   if (!window.location.pathname.startsWith('/cp-request')) {
     enableStickyElements();
@@ -940,7 +940,7 @@ async function loadLazy(doc) {
     if (hash && element) element.scrollIntoView();
 
     // loadFooter(doc.querySelector('footer'));
-    // loadBreadcrumbs(main);
+    loadBreadcrumbs(main);
 
     loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
     loadCSS(`${window.hlx.codeBasePath}/styles/fonts.css`).then(() => {
