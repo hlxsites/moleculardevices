38420fcedde9759ac001b189b35418d4efddb54e
Rajneesh <rajneesh.kumar@nvish.com>
2024-01-29 17:36:44 +0530

    related product fix

--- blocks/related-products/related-products.js
+++ blocks/related-products/related-products.js
@@ -13,12 +13,12 @@ export default async function decorate(block) {
 
   const products = await ffetch('/query-index.json')
     .sheet('products')
-    .filter((product) => allProductTitles.includes(product.identifier || product.h1))
+    .filter((product) => allProductTitles.includes(product.identifier) || allProductTitles.include(product.h1)
     .all();
 
   const categories = await ffetch('/query-index.json')
     .sheet('categories')
-    .filter((category) => relatedCategoriesTitles.includes(category.identifier || category.h1))
+    .filter((category) => relatedCategoriesTitles.includes(category.identifier) || relatedCategoriesTitles.includes(category.h1)
     .all();
 
   const allItems = [...products, ...categories];
