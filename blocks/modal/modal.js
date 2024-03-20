7fb0ba40188602e73f4b3959d2dbd7e47a3c4d80
Rajneesh <rajneesh.kumar@nvish.com>
2024-03-20 13:22:01 +0530

    slas-2024 blog

---
+++ blocks/modal/modal.js
@@ -0,0 +1,136 @@
+import {
+  button, div, iframe, span,
+} from '../../scripts/dom-helpers.js';
+import ffetch from '../../scripts/ffetch.js';
+import { createOptimizedPicture, loadCSS, loadScript } from '../../scripts/lib-franklin.js';
+
+const modalParentClass = 'modal-overlay';
+
+export async function getLatestNewsletter() {
+  return ffetch('/query-index.json')
+    .sheet('resources')
+    .filter((resource) => resource.type === 'Newsletter')
+    .limit(1)
+    .all();
+}
+
+export function hideModal() {
+  const modal = document.querySelector(`.${modalParentClass}`);
+  modal.setAttribute('aria-hidden', true);
+  document.body.classList.remove('no-scroll');
+}
+
+export function showModal() {
+  const newsletterModalOverlay = document.querySelector('.modal-overlay');
+  newsletterModalOverlay.removeAttribute('aria-hidden');
+  document.body.classList.add('no-scroll');
+}
+
+export function showModalWithUrl(url) {
+  const modal = document.querySelector(`.${modalParentClass}`);
+  modal.querySelector('iframe').setAttribute('src', url);
+  setTimeout(() => {
+    modal.setAttribute('aria-hidden', false);
+    document.body.classList.add('no-scroll');
+  }, 200);
+}
+
+export function iframeResizeHandler(formUrl, id, container) {
+  container.querySelector('iframe').addEventListener('load', async () => {
+    if (formUrl) {
+      /* global iFrameResize */
+      iFrameResize({ log: false }, `#${id}`);
+    }
+  });
+}
+
+export function stopProp(e) {
+  e.stopPropagation();
+}
+
+export function triggerModalBtn() {
+  const scrollFromTop = window.scrollY;
+  const midHeightOfViewport = Math.floor(document.body.getBoundingClientRect().height / 2.25);
+
+  const modalBtn = document.getElementById('show-modal');
+
+  if (scrollFromTop > midHeightOfViewport) {
+    if (modalBtn) {
+      modalBtn.click();
+      modalBtn.remove();
+    }
+  }
+}
+
+export async function addNewsletterInParams(formURL) {
+  const latestNewsletter = await getLatestNewsletter();
+  const queryString = window.location.search;
+  let cmpID = new URLSearchParams(queryString).get('cmp');
+  if (!cmpID) cmpID = '';
+  const iframeSrc = `${formURL}?latest_newsletter=${latestNewsletter[0].gatedURL}&cmp=${cmpID}`;
+  return iframeSrc;
+}
+
+export async function decorateModal(formURL, iframeID, modalBody, modalClass, isFormModal) {
+  loadScript('/scripts/iframeResizer.min.js');
+  loadCSS('/blocks/modal/modal.css');
+  const body = document.querySelector('body');
+
+  if (isFormModal) {
+    const modalBtn = button({ id: 'show-modal', style: 'display: none;' }, 'Show Modal');
+    modalBtn.addEventListener('click', showModal);
+    body.append(modalBtn);
+    window.addEventListener('scroll', triggerModalBtn);
+  }
+
+  const formOverlay = div({ 'aria-hidden': true, class: modalParentClass, style: 'display:none;' });
+  formOverlay.addEventListener('click', hideModal);
+  body.append(formOverlay);
+  setTimeout(() => {
+    formOverlay.removeAttribute('style');
+  }, 500);
+
+  const closeBtn = span(
+    { class: 'icon icon-close' },
+    createOptimizedPicture('/icons/close-video.svg', 'Close Video'),
+  );
+  closeBtn.addEventListener('click', hideModal);
+  const innerWrapper = div({ class: ['modal-inner-wrapper', modalClass] }, modalBody, closeBtn);
+  innerWrapper.addEventListener('click', stopProp);
+  formOverlay.append(innerWrapper);
+  iframeResizeHandler(formURL, iframeID, modalBody);
+}
+
+export default async function decorate(block) {
+  const isFormModal = block.classList.contains('form-modal');
+  if (isFormModal) {
+    const modalContent = block.querySelector(':scope > div > div');
+    const link = modalContent.querySelector('p > a:only-child');
+    const formURL = link.href;
+    const iframeID = 'form-modal';
+    const modalBody = div({ class: 'modal-form-col' });
+    link.closest('p').remove();
+
+    const iframeWrapper = div({ class: 'modal-iframe-wrapper' },
+      iframe({
+        src: formURL,
+        id: iframeID,
+        loading: 'lazy',
+        title: 'Modal',
+      }),
+    );
+
+    const headings = block.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    const paragraphs = block.querySelectorAll('p');
+    [...headings].forEach((heading) => {
+      modalBody.append(heading);
+    });
+    [...paragraphs].forEach((para) => {
+      modalBody.append(para);
+    });
+
+    modalBody.appendChild(iframeWrapper);
+
+    decorateModal(formURL, iframeID, modalBody, '', isFormModal);
+  }
+}
--- templates/blog/blog.js
+++ templates/blog/blog.js
@@ -1,77 +1,11 @@
 import {
-  div, img, span, iframe, h3, p, button, h5,
+  div, img, iframe, h3, p, h5,
 } from '../../scripts/dom-helpers.js';
-import { loadScript } from '../../scripts/scripts.js';
-import ffetch from '../../scripts/ffetch.js';
-import { createOptimizedPicture, getMetadata } from '../../scripts/lib-franklin.js';
-
-function showNewsletterModal() {
-  const newsletterModalOverlay = document.querySelector('.newsletter-modal-overlay');
-  newsletterModalOverlay.removeAttribute('aria-hidden');
-  document.body.classList.add('no-scroll');
-}
-
-function hideNewsletterModal() {
-  const newsletterModalOverlay = document.querySelector('.newsletter-modal-overlay');
-  newsletterModalOverlay.setAttribute('aria-hidden', true);
-  document.body.classList.remove('no-scroll');
-}
-
-function stopProp(e) {
-  e.stopPropagation();
-}
-
-function triggerModalBtn() {
-  const scrollFromTop = window.scrollY;
-  const midHeightOfViewport = Math.floor(document.body.getBoundingClientRect().height / 2.25);
-
-  const modalBtn = document.getElementById('show-newsletter-modal');
-
-  if (scrollFromTop > midHeightOfViewport) {
-    if (modalBtn) {
-      modalBtn.click(showNewsletterModal);
-      modalBtn.remove();
-    }
-  }
-}
-
-async function getLatestNewsletter() {
-  return ffetch('/query-index.json')
-    .sheet('resources')
-    .filter((resource) => resource.type === 'Newsletter')
-    .limit(1)
-    .all();
-}
-
-async function setParams(formURL) {
-  const latestNewsletter = await getLatestNewsletter();
-  const queryString = window.location.search;
-  let cmpID = new URLSearchParams(queryString).get('cmp');
-  if (!cmpID) cmpID = '';
-  const iframeSrc = `${formURL}?latest_newsletter=${latestNewsletter[0].gatedURL}&cmp=${cmpID}`;
-  return iframeSrc;
-}
-
-function iframeResizeHandler(formUrl, id, container) {
-  container.querySelector('iframe').addEventListener('load', async () => {
-    if (formUrl) {
-      /* global iFrameResize */
-      iFrameResize({ log: false }, `#${id}`);
-    }
-  });
-}
+import { getMetadata } from '../../scripts/lib-franklin.js';
+import { addNewsletterInParams, decorateModal, iframeResizeHandler } from '../../blocks/modal/modal.js';
 
 async function newsletterModal(formURL, modalIframeID) {
-  const body = document.querySelector('body');
-  const iframeSrc = await setParams(formURL);
-
-  const modalBtn = button({ id: 'show-newsletter-modal', style: 'display: none;' }, 'Show Modal');
-  modalBtn.addEventListener('click', showNewsletterModal);
-  body.append(modalBtn);
-
-  const newsletterOverlay = div({ class: 'newsletter-modal-overlay', 'aria-hidden': true });
-  newsletterOverlay.addEventListener('click', hideNewsletterModal);
-  body.append(newsletterOverlay);
+  const iframeSrc = await addNewsletterInParams(formURL);
 
   const leftColumn = div(
     { class: 'newsletter-left-col newsletter-col' },
@@ -96,23 +30,13 @@ async function newsletterModal(formURL, modalIframeID) {
       ),
     ),
   );
-  const columnsWrapper = div({ class: 'columns columns-2-cols' }, leftColumn, rightColumn);
-  const closeBtn = span(
-    { class: 'icon icon-close newsletter-button-close' },
-    createOptimizedPicture('/icons/close-video.svg', 'Close Video'),
-  );
-  closeBtn.addEventListener('click', hideNewsletterModal);
-  const innerWrapper = div({ class: 'newsletter-inner-wrapper' }, columnsWrapper, closeBtn);
-  innerWrapper.addEventListener('click', stopProp);
-  newsletterOverlay.append(innerWrapper);
+  const modalBody = div({ class: 'columns columns-2-cols' }, leftColumn, rightColumn);
+
+  decorateModal(formURL, modalIframeID, modalBody, 'newsletter-inner-wrapper', true);
   iframeResizeHandler(formURL, modalIframeID, rightColumn);
 }
 
-window.addEventListener('scroll', triggerModalBtn);
-
 export default async function decorate() {
-  loadScript('/scripts/iframeResizer.min.js');
-
   const newsletterMetaData = getMetadata('newsletter-modal');
   const hasNewsletterMetaData = newsletterMetaData.toLowerCase() === 'hide';
 
