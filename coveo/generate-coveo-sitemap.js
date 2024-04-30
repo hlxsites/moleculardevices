5e37e7634c9dfb7e6f228789d2ca88813b0e65ee
Rajneesh <rajneesh.kumar@nvish.com>
2024-04-30 15:36:22 +0530

    technlogy coveo resources

--- coveo/generate-coveo-sitemap.js
+++ coveo/generate-coveo-sitemap.js
@@ -242,7 +242,12 @@ function createCoveoFields(index, icons) {
       result.push(`${item.category}|${itemSearchTitle(item)}`);
       item.mdapplicationsdatacategory = result.join(';');
     }
-
+    if (item.type === 'Technology' && isNotEmpty(item.category)) {
+      const result = [item.category];
+      // isNotEmpty(item.subcategory) && result.push(`${item.category}|${item.subcategory}`);
+      result.push(`${item.category}|${itemSearchTitle(item)}`);
+      item.mdtechnologydatacategory = result.join(';');
+    }
     if (item.md_pagetype === 'Resource' || item.path.endsWith('.pdf')) {
       item.md_title = itemSearchTitle(item);
     }
@@ -267,15 +272,6 @@ function createCoveoFieldsFromRelatedData(index) {
           .join(';');
       }
 
-      // set related application info
-      /* if (isNotEmpty(item.relatedApplications)) {
-        item.md_application = item.relatedApplications.split(',')
-          .map((identifier) => INDENTIFIER_MAPPING.get(identifier.trim()))
-          .filter((application) => !!application)
-          .map(itemSearchTitle)
-          .join(';');
-      } */
-
       if (isNotEmpty(item.relatedApplications)) {
         const relatedApplications = item.relatedApplications.split(',')
           .map((identifier) => INDENTIFIER_MAPPING.get(identifier.trim()))
@@ -284,6 +280,17 @@ function createCoveoFieldsFromRelatedData(index) {
         item.md_application = relatedApplications.map(itemSearchTitle).join(';');
         item.mdapplicationsdatacategory = relatedApplications
           .map((application) => application.mdapplicationsdatacategory)
+          .filter((category) => !!category)
+          .join(';');
+      }
+      if (isNotEmpty(item.relatedTechnologies)) {
+        const relatedTechnologies = item.relatedTechnologies.split(',')
+          .map((identifier) => INDENTIFIER_MAPPING.get(identifier.trim()))
+          .filter((technology) => !!technology);
+
+        item.md_technology= relatedTechnologies.map(itemSearchTitle).join(';');
+        item.mdtechnologydatacategory = relatedTechnologies
+          .map((technology) => technology.mdtechnologydatacategory)
           .filter((category) => !!category)
           .join(';');
       }
@@ -332,8 +339,10 @@ async function writeCoveoSitemapXML(index) {
     xmlData.push(`      <md_img>${item.md_img}</md_img>`);
     xmlData.push(`      <md_product><![CDATA[ ${item.md_product || ''}  ]]></md_product>`);
     xmlData.push(`      <md_application><![CDATA[ ${item.md_application || ''} ]]></md_application>`);
+    xmlData.push(`      <md_technology><![CDATA[ ${item.md_technology || ''} ]]></md_technology>`);
     xmlData.push(`      <mdproductsdatacategory><![CDATA[ ${item.mdproductsdatacategory || ''} ]]></mdproductsdatacategory>`); // TODO
     xmlData.push(`      <mdapplicationsdatacategory><![CDATA[ ${item.mdapplicationsdatacategory || ''} ]]></mdapplicationsdatacategory>`);
+    xmlData.push(`      <mdtechnologydatacategory><![CDATA[ ${item.mdtechnologydatacategory || ''} ]]></mdtechnologydatacategory>`);
     item.md_rfq && xmlData.push(`      <md_rfq>${item.md_rfq}</md_rfq>`);
     xmlData.push(`      <md_country><![CDATA[ ${isNotEmpty(item.md_country) ? item.md_country : ''} ]]></md_country>`); // TODO
     xmlData.push(`      <md_lang><![CDATA[ ${isNotEmpty(item.md_lang) ? item.md_lang : ''} ]]></md_lang>`); // TODO
