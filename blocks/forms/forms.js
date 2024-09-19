329d6b1e9fd22a486cab4d7d824c6675a95a22c0
bluedeepart <deepti.rathor46@gmail.com>
2024-09-19 19:45:57 +0530

    updated the structure

--- blocks/forms/forms.js
+++ blocks/forms/forms.js
@@ -1,26 +1,49 @@
-import { button, div } from '../../scripts/dom-helpers.js';
+import { button, div, h3 } from '../../scripts/dom-helpers.js';
+import { toCamelCase, toClassName } from '../../scripts/lib-franklin.js';
 import { loadScript } from '../../scripts/scripts.js';
 
-/* EXTRACT DATA FROM TABLE  ***/
-async function extractTableData(table) {
-  const tableData = {};
-  table.querySelectorAll('tbody tr').forEach((row) => {
-    const key = row.cells[0].textContent.trim().toLowerCase();
-    const value = row.cells[1].textContent.trim();
-    tableData[key] = value;
-  });
-  return tableData;
+/* Helper functions */
+const keys = ['heading', 'region', 'portalId', 'formId', 'redirectUrl'];
+function getDefaultForKey(key) {
+  switch (key) {
+    case 'region':
+      return 'na1';
+    case 'heading':
+      return '';
+    case 'redirectUrl':
+      return 'https://www.moleculardevices.com/';
+    default:
+      return '';
+  }
 }
 
-/* CREATE HUBSPOT FORM */
-function createHubSpotForm(formConfig) {
+/* extract data from table  */
+async function extractFormData(block) {
+  const blockData = {};
+  [...block.children].forEach((row) => {
+    const key = toCamelCase(row.children[0].textContent.trim().toLowerCase());
+    const value = row.children[1].textContent.trim();
+    blockData[key] = value;
+  });
+
+  keys.forEach((key) => {
+    if (!blockData[key]) {
+      blockData[key] = getDefaultForKey(key);
+    }
+  });
+  return blockData;
+}
+
+/* create hubspot form */
+function createHubSpotForm(formConfig, target) {
+  // console.log(formConfig);
   if (window.hbspt) {
     hbspt.forms.create({ // eslint-disable-line
-      portalId: formConfig.portalid,
-      formId: formConfig.formid,
-      target: `#${formConfig.target}`,
+      portalId: formConfig.portalId,
+      formId: formConfig.formId,
+      target: `#${target}`,
       onFormSubmit: () => {
-        window.location.href = formConfig.redirecturl;
+        window.location.href = formConfig.redirectUrl;
       },
       onFormReady: ($form) => {
         const submitInput = $form.querySelector('input[type="submit"]');
@@ -39,16 +62,24 @@ function createHubSpotForm(formConfig) {
   }
 }
 
+/* load hubspot script */
 function loadHubSpotScript(callback) {
   loadScript('//js.hsforms.net/forms/v2.js', callback, 'text/javascript', true, false);
 }
 
-export default async function decorate(block) {
-  const table = block.querySelector('table');
-  const formConfig = await extractTableData(table);
-  const form = div({ id: formConfig.target, class: 'content' });
-  if (table) {
-    table.replaceWith(form);
-  }
-  loadHubSpotScript(createHubSpotForm.bind(null, formConfig));
+export default async function decorate(block, index) {
+  const formConfig = await extractFormData(block);
+  const formHeading = formConfig.heading || '';
+  const target = toClassName(formHeading) || `hubspot-form-${index}`;
+
+  const form = div(
+    h3(formHeading),
+    div({
+      id: target,
+      class: 'hubspot-form',
+    }),
+  );
+
+  block.replaceWith(form);
+  loadHubSpotScript(createHubSpotForm.bind(null, formConfig, target));
 }
