5035c815b282d3c9bbe7c98aee47d68fe1965415
dev-rajneeshkumar <rajneesh.kumar@nvish.com>
2024-11-07 11:58:58 +0530

    merge blog and articles

--- blocks/card-list/card-list.js
+++ blocks/card-list/card-list.js
@@ -246,9 +246,29 @@ const VARIANTS = {
     cardRenderer: blogCardRender,
 
     async getData() {
-      return ffetch('/query-index.json')
+      let data = [];
+      const publications = await ffetch('/query-index.json')
+        .sheet('publications')
+        .filter((resource) => resource.publicationType === 'Full Article')
+        .all();
+
+      const blogs = await ffetch('/query-index.json')
         .sheet('blog')
         .all();
+
+      data = [...publications, ...blogs];
+
+      data.sort((x, y) => {
+        if (x.date > y.date) {
+          return -1;
+        }
+        if (x.date < y.date) {
+          return 1;
+        }
+        return 0;
+      });
+
+      return data;
     },
 
     getCategories(item) {
