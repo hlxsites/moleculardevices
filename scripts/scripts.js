d1baeb26f24d533cc6c55703718b837816a48511
bluedeepart <deepti.rathor46@gmail.com>
2024-11-11 20:16:54 +0530

    initial commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -752,12 +752,25 @@ function addHreflangTags() {
  * @param {Element} iframeID The iframe id
  * @param {Element} root The parent element of iframe
  */
-export function iframeResizeHandler(iframeURL, iframeID, root) {
-  loadScript('/scripts/iframeResizer.min.js');
-  root.querySelector('iframe').addEventListener('load', () => {
-    if (iframeURL) {
+// export function iframeResizeHandler(iframeURL, iframeID, root) {
+//   loadScript('/scripts/iframeResizer.min.js');
+//   root.querySelector('iframe').addEventListener('load', () => {
+//     if (iframeURL) {
+//       /* global iFrameResize */
+//       iFrameResize({ log: false, checkOrigin: false, heightCalculationMethod: 'bodyScroll' }, `#${iframeID}`);
+//     }
+//   });
+// }
+export function iframeResizeHandler(formUrl, id, container) {
+  const resizerPromise = new Promise((resolve) => {
+    loadScript('/scripts/iframeResizer.min.js', () => { resolve(); });
+  });
+
+  container.querySelector('iframe').addEventListener('load', async () => {
+    if (formUrl) {
+      await resizerPromise;
       /* global iFrameResize */
-      iFrameResize({ log: false, checkOrigin: false, heightCalculationMethod: 'bodyScroll' }, `#${iframeID}`);
+      iFrameResize({ log: false }, `#${id}`);
     }
   });
 }
