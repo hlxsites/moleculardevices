fdf3aab21d190e46a0f12aaf9598e8ccb6ed4c59
dev-rajneeshkumar <rajneesh.kumar@nvish.com>
2024-12-05 15:34:03 +0530

    remove video schema

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -678,9 +678,9 @@ function addPageSchema() {
       };
     }
 
-    /* 
-     if (type === 'Videos and Webinars') {
-      const vidyardLink = document.querySelector('a[href*="vidyard.com"], a[href*="vids.moleculardevices.com"]').href;
+    /* if (type === 'Videos and Webinars') {
+      const vidyardLink = document.querySelector('a[href*="vidyard.com"],
+      a[href*="vids.moleculardevices.com"]').href;
       const vidyardId = vidyardLink.split('/').pop();
       const embedHref = `https://play.vidyard.com/${vidyardId}?disable_popouts=1&v=4.3.15&autoplay=1&type=lightbox`;
 
