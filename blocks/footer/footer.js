e21df7f9a62b61e0fe46f2eaf01b67ffefd0ffb0
bluedeepart <deepti.rathor46@gmail.com>
2024-12-17 17:55:55 +0530

    fixed UI issues

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -139,7 +139,6 @@ async function buildNewsletter(container) {
       class: 'enewsletter-wrapper',
       loading: 'lazy',
     },
-    // h3('eNEWSLETTER'),
     div(
       {
         class: 'hubspot-form',
