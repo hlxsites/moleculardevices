5ef62e0d53c267ba23df85759c96737e1f4822f8
bluedeepart <deepti.rathor46@gmail.com>
2025-01-07 17:00:16 +0530

    fixed lint issues

--- blocks/forms/forms.js
+++ blocks/forms/forms.js
@@ -86,6 +86,5 @@ export default async function decorate(block, index) {
 
   block.innerHTML = '';
   block.appendChild(form);
-  console.log(formConfig);
   loadHubSpotScript(createHubSpotForm.bind(null, formConfig, target, formType));
 }
