6ee501eed7b3584a13e76812fad423614b1e4118
bluedeepart <deepti.rathor46@gmail.com>
2025-01-29 15:24:15 +0530

    reverted: update cmp cases according to module

--- blocks/get-in-touch/get-in-touch.js
+++ blocks/get-in-touch/get-in-touch.js
@@ -1,58 +1,54 @@
-import { div } from '../../scripts/dom-helpers.js';
 import ffetch from '../../scripts/ffetch.js';
 import { getCookie } from '../../scripts/scripts.js';
-import { getFormId } from '../forms/formHelper.js';
-import { createHubSpotForm, loadHubSpotScript } from '../forms/forms.js';
 
-// const CONTACT_CMP_ID = getCookie('cmp') || new URLSearchParams(window.location.search).get('cmp')
-//  || '701Rn00000S2zk6IAB';
-const CONTACT_CMP_ID = getCookie('cmp') || new URLSearchParams(window.location.search).get('cmp') || '701Rn00000OJ0zYIAT';
-const formType = 'get-in-touch';
-const pathName = `${window.location.origin}/contact`;
+let DEFAULT_CMP = '';
 let REGION = new URLSearchParams(window.location.search).get('region');
+const COMMENTS = 'comments';
 
-const formConfig = {
-  formId: getFormId(formType),
-  cmp: CONTACT_CMP_ID,
-  qdc: 'Call',
-  productPrimaryApplication: 'General Inquiry Form Submission',
-  redirectUrl: new URL(`?msg=success&region=${REGION}&cmp=${CONTACT_CMP_ID}`, pathName),
-};
+function hubSpotFinalUrl(hubspotUrl, paramName) {
+  const hubUrl = new URL(hubspotUrl.href);
+  const { searchParams } = hubUrl;
+  const cmp = getCookie('cmp') || searchParams.get('cmp');
+  const returnURL = new URLSearchParams(decodeURIComponent(searchParams.get('return_url')));
+  const queryParams = new URLSearchParams(window.location.search);
 
-function updateParams(params) {
-  const returnUrlInput = document.querySelector("input[name='return_url']");
-  const baseRedirectUrl = new URL(formConfig.redirectUrl, pathName);
+  searchParams.delete('cmp');
+  searchParams.delete('region');
+  searchParams.delete('return_url');
 
-  Object.entries(params).forEach(([key, value]) => {
-    if (value || value === 'sales') {
-      baseRedirectUrl.searchParams.set(key, value);
-      if (value === 'general') {
-        baseRedirectUrl.searchParams.delete(key);
-      }
-    } else {
-      baseRedirectUrl.searchParams.delete(key);
+  returnURL.set('region', queryParams.get('region') || REGION);
+
+  if (paramName === 'general') {
+    searchParams.delete(COMMENTS);
+  }
+  if (paramName === COMMENTS) {
+    searchParams.set(paramName, 'Sales');
+  }
+
+  const searchPatamsStr = searchParams.toString() ? `&${(searchParams.toString())}` : '';
+  const queryStr = `?return_url=${returnURL.toString()}${searchPatamsStr}&cmp=${cmp || DEFAULT_CMP}`;
+  return new URL(`${hubspotUrl.pathname}${queryStr}`, hubspotUrl);
+}
+
+function createForm(block, hubspotUrl) {
+  const hubspotIframeWrapper = document.createElement('div');
+  const hubspotIframe = document.createElement('iframe');
+  hubspotIframeWrapper.className = 'hubspot-iframe-wrapper get-in-touch-form';
+  hubspotIframe.setAttribute('loading', 'lazy');
+  hubspotIframeWrapper.appendChild(hubspotIframe);
+  hubspotUrl.parentNode.replaceChild(hubspotIframeWrapper, hubspotUrl);
+
+  const observer = new IntersectionObserver((entries) => {
+    if (entries.some((e) => e.isIntersecting)) {
+      observer.disconnect();
+      const hubUrl = hubSpotFinalUrl(hubspotUrl, 'region');
+      hubspotUrl.href = hubUrl.href;
+      hubspotIframe.src = hubspotUrl.href;
     }
   });
-
-  formConfig.redirectUrl = baseRedirectUrl.pathname + baseRedirectUrl.search;
-  returnUrlInput.value = new URL(formConfig.redirectUrl, pathName);
+  observer.observe(block);
 }
 
-/* create form */
-function createForm(block) {
-  const contactFormID = 'get-in-touch-form';
-  const hubspotIframeWrapper = div(
-    { class: 'hubspot-form-wrapper' },
-    div({
-      class: 'show-label',
-      id: contactFormID,
-    }));
-
-  loadHubSpotScript(createHubSpotForm.bind(null, formConfig, contactFormID, formType));
-  block.firstElementChild.firstElementChild.appendChild(hubspotIframeWrapper);
-}
-
-/* create google map */
 function createMap(block, mapUrl) {
   const mapIframeWrapper = document.createElement('div');
   const mapIframe = document.createElement('iframe');
@@ -70,23 +66,31 @@ function createMap(block, mapUrl) {
   observer.observe(block);
 }
 
-function scrollToForm(link, region) {
-  const hubspotFormWrapper = document.getElementById('get-in-touch-form');
-  const getInTouchBlock = document.querySelector('.get-in-touch');
-  const getInTouchInterestsSelect = hubspotFormWrapper.querySelector("select[name='get_in_touch_interests']");
+function regenerateForm(hubspotUrl, params) {
+  const hubspotIframe = document.querySelector('.get-in-touch-form');
+  if (hubspotUrl) {
+    const hubUrl = hubSpotFinalUrl(hubspotUrl, params);
+    hubspotUrl.href = hubUrl.href;
+    hubspotIframe.querySelector('iframe').setAttribute('src', hubspotUrl);
+  }
+}
 
-  if (hubspotFormWrapper) {
+function scrollToForm(link, hubspotUrl, region) {
+  const hubspotIframe = document.querySelector('.get-in-touch-form');
+  const getInTouchBlock = document.querySelector('.get-in-touch');
+  if (hubspotIframe && hubspotUrl) {
+    const url = new URLSearchParams(hubspotUrl.href);
+    if (!DEFAULT_CMP) {
+      DEFAULT_CMP = url.get('cmp');
+    }
     let params = 'general';
     if (link && link.getAttribute('title') === 'Sales Inquiry Form') {
-      params = 'sales';
-      getInTouchInterestsSelect.value = 'Sales';
-      getInTouchInterestsSelect.dispatchEvent(new Event('change', { bubbles: true }));
-    } else {
-      getInTouchInterestsSelect.selectedIndex = 0;
+      params = COMMENTS;
     }
-    updateParams({ comments: params, region });
+    const hubUrl = hubSpotFinalUrl(hubspotUrl, params, region);
+    hubspotUrl.href = hubUrl.href;
+    hubspotIframe.querySelector('iframe').setAttribute('src', hubspotUrl);
   }
-
   window.scroll({
     top: getInTouchBlock.offsetTop - 100,
     behavior: 'smooth',
@@ -95,6 +99,7 @@ function scrollToForm(link, region) {
 
 export default async function decorate(block) {
   const queryParams = new URLSearchParams(window.location.search);
+  const hubspotUrl = block.querySelector('[href*="https://info.moleculardevices.com"]');
   const mapUrl = block.querySelector('[href*="https://maps.google.com"]');
 
   /* set success msg */
@@ -102,7 +107,7 @@ export default async function decorate(block) {
     const getInTouchBlock = document.querySelector('.get-in-touch');
     const successMsg = block.lastElementChild.firstElementChild;
     successMsg.classList.add('hubspot-success');
-    block.firstElementChild.firstElementChild.replaceWith(successMsg);
+    hubspotUrl.closest('div').replaceWith(successMsg);
     block.lastElementChild.remove();
     createMap(block, mapUrl);
     setTimeout(() => {
@@ -115,8 +120,7 @@ export default async function decorate(block) {
     }, 1000);
   } else {
     block.lastElementChild.remove(); // success message we don't need for this case
-    block.firstElementChild.firstElementChild.lastElementChild.remove();
-    createForm(block);
+    createForm(block, hubspotUrl);
     createMap(block, mapUrl);
   }
 
@@ -127,29 +131,13 @@ export default async function decorate(block) {
       link.addEventListener('click', () => {
         const regionName = link.hash.split('#')[1] || queryParams.get('region');
         REGION = regionName;
-
-        // update region
-        updateParams({ region: REGION });
-
-        // set default country value to FIND A LOCAL DISTRIBUTOR
+        regenerateForm(hubspotUrl, '');
         setTimeout(() => {
           document.getElementById('country').selectedIndex = 1;
         }, 500);
       });
     });
   }
-
-  /* scroll to form on click of inquiry links */
-  const inquiryLinks = ['General Inquiry Form', 'Sales Inquiry Form', 'Contact Local Team', 'Service plans/warranty'];
-  const links = document.querySelectorAll('a[title]');
-  links.forEach((link) => {
-    if (inquiryLinks.includes(link.getAttribute('title'))) {
-      link.addEventListener('click', (event) => {
-        event.preventDefault();
-        scrollToForm(link, REGION);
-      }, false);
-    }
-  });
 
   /* get region on country change */
   const distributors = await ffetch('/contact/local-distibutors.json').withFetch(fetch).all();
@@ -159,7 +147,6 @@ export default async function decorate(block) {
   if (window.location.pathname === '/contact-search') {
     REGION = distributors.filter(
       (dist) => dist.DisplayCountry === countrySelect.value)[0].Region.toLowerCase();
-    updateParams({ region: REGION });
   } else {
     document.getElementById('country').selectedIndex = 1;
   }
@@ -170,7 +157,19 @@ export default async function decorate(block) {
       const countryObj = distributors.filter(
         (dist) => dist.DisplayCountry === countrySelect.value)[0].Region.toLowerCase();
       REGION = countryObj || queryParams.get('region');
-      updateParams({ region: REGION });
+      regenerateForm(hubspotUrl, '');
     });
   }
+
+  /* scroll to form on click of inquiry links */
+  const inquiryLinks = ['General Inquiry Form', 'Sales Inquiry Form', 'Contact Local Team', 'Service plans/warranty'];
+  const links = document.querySelectorAll('a[title]');
+  links.forEach((link) => {
+    if (inquiryLinks.includes(link.getAttribute('title'))) {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        scrollToForm(link, hubspotUrl, REGION);
+      }, false);
+    }
+  });
 }
