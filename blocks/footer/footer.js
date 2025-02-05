2bb6a35fe8db2dcd4dfa0f303a6972a95a122d05
bluedeepart <deepti.rathor46@gmail.com>
2025-02-05 14:07:15 +0530

    added alt value

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -192,7 +192,7 @@ export default async function decorate(block) {
 
   const currentYear = new Date().getFullYear();
   const siteLogoPath = '/images/header-menus/logo.svg';
-  const placeholderImage = createOptimizedPicture(siteLogoPath);
+  const placeholderImage = createOptimizedPicture(siteLogoPath, 'Molecular Devices');
   placeholderImage.querySelector('img').width = 210;
   placeholderImage.querySelector('img').height = 54;
   const footerSiteLogo = p(
