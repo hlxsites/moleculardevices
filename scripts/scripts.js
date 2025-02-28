ba189f8c34800d125b3cf1e37436e00d1140ca41
bluedeepart <deepti.rathor46@gmail.com>
2025-02-28 12:38:10 +0530

    added popup

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -271,16 +271,18 @@ function getWistiaVideoId(url) {
 export function embedVideoTest(videoID, block) {
   if (videoID) {
     block.innerHTML = `
-    <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
-      <div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:80%;">
-        <div class="wistia_embed wistia_async_${videoID} videoFoam=true email=userEmail playerColor=008da9"
-          style="height:80%;position:relative;width:80%">
-          <div class="wistia_swatch"
-            style="height:80%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;
-            transition:opacity 200ms;width:100%;">
-            <img src="https://fast.wistia.com/embed/medias/${videoID}/swatch"
-              style="filter:blur(5px);height:100%;object-fit:contain;width:100%;"
-              alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" />
+    <div class='video-wrapper'>
+      <div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
+        <div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
+          <div class="wistia_embed wistia_async_${videoID} videoFoam=true email=userEmail playerColor=008da9 popover=true"
+            style="height:100%;position:relative;width:100%">
+            <div class="wistia_swatch"
+              style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;
+              transition:opacity 200ms;width:100%;">
+              <img src="https://fast.wistia.com/embed/medias/${videoID}/swatch"
+                style="filter:blur(5px);height:100%;object-fit:contain;width:100%;"
+                alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" />
+            </div>
           </div>
         </div>
       </div>
