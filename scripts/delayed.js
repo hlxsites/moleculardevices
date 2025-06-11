cebec26948c981c5867716b2ff3906c25244d1ea
dev-rajneeshkumar <rajneesh.kumar@nvish.com>
2025-06-11 18:13:52 +0530

    it changes

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -135,8 +135,8 @@ if (!isSidekickLibrary) {
   if (!window.location.hostname.includes('localhost') && !document.location.hostname.includes('.aem.page')) {
     loadGTM();
   }
- if (!window.location.hostname.includes('localhost') && !document.location.hostname.match('.aem.page') && !document.location.hostname.match('www.moleculardevices.com.cn') && typeof OnetrustActiveGroups !== 'undefined' && OnetrustActiveGroups.includes('C0002')) {
+  if (!window.location.hostname.includes('localhost') && !document.location.hostname.match('.aem.page') && !document.location.hostname.match('www.moleculardevices.com.cn') && typeof OnetrustActiveGroups !== 'undefined' && OnetrustActiveGroups.includes('C0002')) {
     LoadDriftWidget();
   }
-
 }
+ 
