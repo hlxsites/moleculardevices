1a6f9287e7d7a8e2d6c84fdbfb3e66f4a873eb46
bluedeepart <deepti.rathor46@gmail.com>
2025-08-22 10:10:57 +0530

    fixed wave issue

--- blocks/customer-breakthrough-carousel/customer-breakthrough-carousel.js
+++ blocks/customer-breakthrough-carousel/customer-breakthrough-carousel.js
@@ -1,8 +1,9 @@
 import ffetch from '../../scripts/ffetch.js';
-import { fetchPlaceholders, getMetadata } from '../../scripts/lib-franklin.js';
+import { createOptimizedPicture, fetchPlaceholders, getMetadata } from '../../scripts/lib-franklin.js';
 import { createCarousel } from '../carousel/carousel.js';
 import { createCard } from '../card/card.js';
 import { addViewAllCTA } from '../latest-resources/latest-resources.js';
+import { div } from '../../scripts/dom-helpers.js';
 
 const placeholders = await fetchPlaceholders();
 
@@ -17,9 +18,16 @@ async function getCBData() {
 
 export default async function decorate(block) {
   const resources = await getCBData();
+  const waveImage = createOptimizedPicture('/images/wave-footer-bg-top.png', 'wave', false, [
+    { media: '(min-width: 992px)', width: '1663' },
+    { width: '900' },
+  ]);
   if (resources.length < 3) {
-    block.parentElement.previousElementSibling.remove();
-    block.parentElement.remove();
+    // block.parentElement.previousElementSibling.remove();
+    // block.parentElement.remove();
+    block.closest('.section').previousElementSibling.classList.add('wave-section');
+    block.closest('.section').previousElementSibling.appendChild(div({ class: 'wave' }, waveImage));
+    block.closest('.section').remove();
     return;
   }
 
