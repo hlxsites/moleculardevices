80efd12ac36aa4544c34f52d4174b427d0f90c7a
dev-deepti <deepti.devi@nvish.com>
2025-09-26 11:32:53 +0530

    fixed date picker

--- blocks/forms/forms.js
+++ blocks/forms/forms.js
@@ -125,6 +125,7 @@ export default async function decorate(block) {
         const minDate = new Date(getMetadata('event-start')).toISOString().split('T')[0];
 
         if (!hasBookTimeOption) {
+          dateInput?.closest('.hs-form-field').remove();
           meetingDateInput?.closest('.hs-form-field').remove();
           meetingTimeInput?.closest('.hs-form-field').remove();
         } else {
