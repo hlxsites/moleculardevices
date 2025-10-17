859f4596ee284460f78e7093913fed9f7244145b
dev-deepti <deepti.devi@nvish.com>
2025-10-17 14:44:32 +0530

    updated css

--- templates/blog/blog.js
+++ templates/blog/blog.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-cycle */
 import {
-  div, img, h3, p, h5, strong, i, a,
+  div, img, h3, p, strong, i, a,
 } from '../../scripts/dom-helpers.js';
 import ffetch from '../../scripts/ffetch.js';
 import { createHubSpotForm, loadHubSpotScript } from '../../blocks/forms/forms.js';
