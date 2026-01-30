501610e3fbbdaf4ae26c301c26d94075a6006fb2
dev-deepti <deepti.devi@nvish.com>
2026-01-30 11:10:03 +0530

    fixed vidyard video

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -300,7 +300,7 @@ export function decorateLinks(main) {
       const closestButtonContainer = link.closest('.button-container');
       if (link.closest('.block.cards')
         || (closestButtonContainer && closestButtonContainer.querySelector('strong,em'))) {
-        videoButton(link.closest('div'), link, url);
+        videoButton(link.closest('div, article'), link, url);
       } else {
         const up = link.parentElement;
         const hasAutoplay = link.closest('.block.autoplay-video');
