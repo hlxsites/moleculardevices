b6c5ae41fdfa5f2a2494ae70030a4ab512134900
bluedeepart <deepti.rathor46@gmail.com>
2026-03-20 18:57:27 +0530

    fixed SFDC value for RFQ form

--- blocks/quote-request/quote-request.js
+++ blocks/quote-request/quote-request.js
@@ -196,8 +196,6 @@ async function loadIframeForm(data, type) {
           .querySelector('meta[name="related-products"]')
           .getAttribute('content');
         tab = relatedProducts && relatedProducts.trim().length > 0 ? relatedProducts : data.title;
-        tab = tab.replace('iD3', 'iD3s');
-        tab = tab.replace('iD5', 'iD5e');
         sfdcPrimaryApplication = tab;
 
         const mainProduct = await getRFQDataByTitle(relatedProducts.split(',')[0].trim());
@@ -251,6 +249,9 @@ async function loadIframeForm(data, type) {
   if (data.path) {
     hubSpotQuery.website = `https://www.moleculardevices.com${data.path}`;
   }
+
+  tab = tab.replace('iD3', 'iD3s');
+  tab = tab.replace('iD5', 'iD5e');
 
   const formWrapper = div(
     h3('Request Quote or Information for:'),
