4bd6b3ff01b62b080580a1e97080aea8702ae8cc
dev-rajneeshkumar <rajneesh.kumar@nvish.com>
2026-03-30 15:42:15 +0530

    new coveo file

---
+++ scripts/build-js.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { minify } from 'terser';
+import fs from 'fs';
+
+const files = [
+  { in: './scripts/lib-franklin.js', out: './scripts/lib-franklin.min.js' },
+  { in: './scripts/scripts.js', out: './scripts/scripts.min.js' },
+];
+
+(async () => {
+  // eslint-disable-next-line no-restricted-syntax
+  for (const file of files) {
+    const code = fs.readFileSync(file.in, 'utf-8');
+
+    // eslint-disable-next-line no-await-in-loop
+    const result = await minify(code, {
+      compress: true,
+      mangle: false,
+      format: {
+        beautify: false,
+        semicolons: true,
+        comments: false,
+      },
+    });
+
+    fs.writeFileSync(file.out, result.code);
+    // eslint-disable-next-line no-console
+    console.log(`Minified: ${file.out}`);
+  }
+})();
