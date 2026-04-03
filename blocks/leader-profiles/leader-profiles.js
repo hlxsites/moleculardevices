d7bcaf711e888015f6cefce8de894d65a8815dc2
bluedeepart <deepti.rathor46@gmail.com>
2026-04-03 18:31:02 +0530

    added lib-franklin min file

--- blocks/leader-profiles/leader-profiles.js
+++ blocks/leader-profiles/leader-profiles.js
@@ -53,11 +53,8 @@ class LeadershipModal {
         allImages.forEach((image) => {
           const PictureEl = image.closest('.leader-profiles-card-image');
           if (PictureEl) {
-            PictureEl.style.minHeight = `${
-              PictureEl.clientWidth < PictureEl.clientHeight
-                ? PictureEl.clientWidth
-                : PictureEl.clientHeight
-            }px`;
+            PictureEl.style.minHeight = `${PictureEl.clientWidth < PictureEl.clientHeight
+              ? PictureEl.clientWidth : PictureEl.clientHeight}px`;
             image.width = PictureEl.clientWidth;
             image.height = PictureEl.clientHeight;
           }
