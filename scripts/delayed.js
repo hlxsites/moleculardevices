08f17c770425186fba1d393ac145a218e62bff84
dev-rajneeshkumar <rajneesh.kumar@nvish.com>
2026-04-10 17:03:34 +0530

    ai scipt updated for pae type

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,34 +6,7 @@ const isSidekickLibrary = (window.location.href === 'about:srcdoc');
 window.intercomSettings = {
     app_id: "kahrndo3"
   };
-
-(function(){
-  var w=window;
-  var ic=w.Intercom;
-  if(typeof ic==="function"){
-    ic('reattach_activator');
-    ic('update',w.intercomSettings);
-  } else {
-    var d=document;
-    var i=function(){i.c(arguments);};
-    i.q=[];
-    i.c=function(args){i.q.push(args);};
-    w.Intercom=i;
-    var l=function(){
-      var s=d.createElement('script');
-      s.type='text/javascript';
-      s.async=true;
-      s.src='https://widget.intercom.io/widget/kahrndo3';
-      var x=d.getElementsByTagName('script')[0];
-      x.parentNode.insertBefore(s,x);
-    };
-    if(document.readyState==='complete'){l();}
-    else if(w.attachEvent){w.attachEvent('onload',l);}
-    else{w.addEventListener('load',l,false);}
-  }
-})
-
-(function () {
+  (function () {
   function getPageContext() {
     const path = window.location.pathname.toLowerCase();
 
@@ -87,10 +60,33 @@ window.intercomSettings = {
   window.addEventListener("load", function () {
     setTimeout(updateIntercomContext, 1000);
   });
-})
-();
+})();
 
-
+(function(){
+  var w=window;
+  var ic=w.Intercom;
+  if(typeof ic==="function"){
+    ic('reattach_activator');
+    ic('update',w.intercomSettings);
+  } else {
+    var d=document;
+    var i=function(){i.c(arguments);};
+    i.q=[];
+    i.c=function(args){i.q.push(args);};
+    w.Intercom=i;
+    var l=function(){
+      var s=d.createElement('script');
+      s.type='text/javascript';
+      s.async=true;
+      s.src='https://widget.intercom.io/widget/kahrndo3';
+      var x=d.getElementsByTagName('script')[0];
+      x.parentNode.insertBefore(s,x);
+    };
+    if(document.readyState==='complete'){l();}
+    else if(w.attachEvent){w.attachEvent('onload',l);}
+    else{w.addEventListener('load',l,false);}
+  }
+})();
 // end of intercom script for the chatbot
 /*
 IPStack Integration to get specific user information
